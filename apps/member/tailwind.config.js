9493cd824ebf5160f971c687801bd6f6415c3b88
gwansikk <seorit98@gmail.com>
2024-01-15 18:32:29 +0900

    feat(member): TailwindCSS 환경설정

---
+++ apps/member/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1280px'
+        }
+      },
+      colors: {
+        'clab-main': '#292c33',
+        'clab-main-light': '#7c7d7f',
+        'clab-main-dark': '#121415'
+      }
+    }
+  },
+  plugins: []
+};
--- apps/member/vite.config.ts
+++ apps/member/vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@api', replacement: 'src/api' },
+      { find: '@assets', replacement: 'src/assets' },
+      { find: '@components', replacement: 'src/components' },
+      { find: '@constants', replacement: 'src/constants' },
+      { find: '@hooks', replacement: 'src/hooks' },
+      { find: '@mocks', replacement: 'src/mocks' },
+      { find: '@pages', replacement: 'src/pages' },
+      { find: '@router', replacement: 'src/router' },
+      { find: '@store', replacement: 'src/store' },
+      { find: '@styles', replacement: 'src/styles' },
+      { find: '@types', replacement: 'src/types' },
+      { find: '@utils', replacement: 'src/utils' }
+    ]
+  }
 });
