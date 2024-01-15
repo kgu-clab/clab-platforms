94c336cb33c759f007f8a91a231a3afd21ef69d2
gwansikk <seorit98@gmail.com>
2024-01-15 20:20:11 +0900

    chore(member): path alias

--- apps/member/vite.config.ts
+++ apps/member/vite.config.ts
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@api', replacement: 'src/api' },
-      { find: '@assets', replacement: 'src/assets' },
-      { find: '@components', replacement: 'src/components' },
-      { find: '@constants', replacement: 'src/constants' },
-      { find: '@hooks', replacement: 'src/hooks' },
-      { find: '@mocks', replacement: 'src/mocks' },
-      { find: '@pages', replacement: 'src/pages' },
-      { find: '@router', replacement: 'src/router' },
-      { find: '@store', replacement: 'src/store' },
-      { find: '@styles', replacement: 'src/styles' },
-      { find: '@types', replacement: 'src/types' },
-      { find: '@utils', replacement: 'src/utils' }
-    ]
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@mocks': path.resolve(__dirname, './src/mocks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    }
   }
 });
