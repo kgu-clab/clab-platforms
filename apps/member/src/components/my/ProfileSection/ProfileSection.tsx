44a22b816090154af375716535d28cca9fe441d9
Jeong-Ag <kjungu1105@gmail.com>
2024-01-17 19:53:41 +0900

    feat: MyPage 마이그레이션

---
+++ apps/member/src/components/my/ProfileSection/ProfileSection.tsx
@@ -0,0 +1,71 @@
+import Button from '@components/common/Button/Button';
+import Image from '@components/common/Image/Image';
+import Section from '@components/common/Section/Section';
+
+interface ProfileSectionProps {
+  data: {
+    image: string;
+    name: string;
+    id: number;
+    interests: string;
+    phone: string;
+    email: string;
+    githubUrl: string;
+    address: string;
+  }[];
+}
+
+const ProfileSection = ({ data }: ProfileSectionProps) => {
+  const profileData = data[0];
+  return (
+    <Section>
+      <Section.Header title="나의 정보">
+        <div className="flex gap-2">
+          <Button color="orange" children="수정" className="px-1" />
+          <Button color="red" children="로그아웃" className="px-1" />
+        </div>
+      </Section.Header>
+      <Section.Body className="flex flex-col">
+        <div className="flex flex-col text-center items-center">
+          <Image
+            src={profileData.image}
+            alt="프로필 이미지"
+            className="rounded-full m-auto"
+            width={32}
+            height={32}
+          />
+
+          <div className="mt-2">
+            <p className="text-xl font-bold">{profileData.name}</p>
+            <p className="text-sm font-semibold text-gray-500">
+              {profileData.id}
+            </p>
+          </div>
+        </div>
+        <div className="mt-4 space-y-4">
+          <Info label="분야">{profileData.interests}</Info>
+          <Info label="연락처">{profileData.phone}</Info>
+          <Info label="이메일">{profileData.email}</Info>
+          <Info label="Github">{profileData.githubUrl}</Info>
+          <Info label="주소">{profileData.address}</Info>
+        </div>
+      </Section.Body>
+    </Section>
+  );
+};
+
+interface InfoProps {
+  label: string;
+  children: React.ReactNode;
+}
+
+const Info = ({ label, children }: InfoProps) => {
+  return (
+    <div className="flex text-lg">
+      <p className="w-24 font-semibold">{label}</p>
+      <p className="grow">{children}</p>
+    </div>
+  );
+};
+
+export default ProfileSection;
