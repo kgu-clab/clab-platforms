e163f5e056b686c39dd5991dc524891ba2076306
gwansikk <seorit98@gmail.com>
2024-01-17 12:29:50 +0900

    feat: PATH_FINDER

--- apps/member/src/constants/path.ts
+++ apps/member/src/constants/path.ts
@@ -5,7 +5,14 @@ export const PATH = {
   CALENDER: '/calendar',
   ACTIVITY: '/activity',
   COMMUNITY: '/community',
+  NEWS: '/news',
+  BLOG: '/blog',
   LIBRARY: '/library',
   SUPPORT: '/support',
-  SITEMAP: '/sitemap'
+  SITEMAP: '/sitemap',
+};
+
+export const PATH_FINDER = {
+  NEWS_POST: (id: number) => `${PATH.NEWS}/${id}`,
+  BLOG_POST: (id: number) => `${PATH.BLOG}/${id}`,
 };
