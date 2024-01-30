3cf506d2b7f2cd90071bc6bf6d633bb398c27cf4
gwansikk <seorit98@gmail.com>
2024-01-31 02:56:38 +0900

    chore(auth): set custom path

--- apps/auth/tailwind.config.ts
+++ apps/auth/tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from 'tailwindcss';
+import path from 'path';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{ts,jsx,tsx}',
+    path.join(path.dirname(require.resolve('@clab/design-system')), '**/*.js'),
   ],
   theme: {
     extend: {
