5535d4cbee8f2b1860a9b4c251624611d1c05f85
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-02-03 00:38:10 +0900

    chore: tanstack-query로 전환

--- apps/member/src/hooks/queries/queryClient.ts
+++ apps/member/src/hooks/queries/queryClient.ts
@@ -1,10 +1,10 @@
-import { QueryClient } from 'react-query';
+import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
-      refetchOnWindowFocus: false
-    }
-  }
+      refetchOnWindowFocus: false,
+    },
+  },
 });
