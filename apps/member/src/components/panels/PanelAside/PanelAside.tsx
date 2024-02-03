a84b8b28f257f5a873ba68fd488d5ae136cf7f41
Jeong-Ag <kjungu1105@gmail.com>
2024-02-04 01:53:07 +0900

    feat: 패널 알림 API 연동

--- apps/member/src/components/panels/PanelAside/PanelAside.tsx
+++ apps/member/src/components/panels/PanelAside/PanelAside.tsx
@@ -6,23 +6,24 @@ import BookPanel from '../BookPanel/BookPanel';
 import AccountPanel from '../AccountPanel/AccountPanel';
 import SetupPanel from '../SetupPanel/SetupPanel';
 import { account } from '@mocks/mocks';
+import { useMyNotifications } from '@hooks/queries';
 
 const PanelAside = () => {
+  const { data: myNotificationsData } = useMyNotifications();
+
   return (
     <aside className="hidden xl:w-1/4 xl:block">
-      {account.map(
-        ({ name, image, createAt, alerts, activity, books }, index) => (
-          <div key={index} className="space-y-4">
-            <ProfilePanel name={name} image={image} createAt={createAt} />
-            <AttendanceFairyPanel />
-            <AlarmPanel data={alerts} />
-            <ActivityPanel data={activity} />
-            <BookPanel data={books} />
-            <AccountPanel />
-            <SetupPanel />
-          </div>
-        ),
-      )}
+      {account.map(({ name, image, createAt, activity, books }, index) => (
+        <div key={index} className="space-y-4">
+          <ProfilePanel name={name} image={image} createAt={createAt} />
+          <AttendanceFairyPanel />
+          <AlarmPanel data={myNotificationsData.items} />
+          <ActivityPanel data={activity} />
+          <BookPanel data={books} />
+          <AccountPanel />
+          <SetupPanel />
+        </div>
+      ))}
     </aside>
   );
 };
