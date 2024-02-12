b24df26d844affbe0f0d82cc97a0a5c5aea0f54b
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-02-13 02:35:34 +0900

    refactor: CommunitySection 코드 개선
    - Wrapper 컴포넌트를 기본 컴포넌트로 변경
    - 렌더링 되는 컴포넌트의 이름을 List로 변경

--- apps/member/src/components/main/CommunitySection/CommunitySection.tsx
+++ apps/member/src/components/main/CommunitySection/CommunitySection.tsx
@@ -7,13 +7,25 @@ import { HireItem } from '@type/hire';
 import { NewsItem } from '@type/news';
 
 interface CommunitySectionProps {
+  children: React.ReactNode;
+}
+
+interface CommunitySectionListProps {
   title: string;
   to: string;
   number?: number;
   data: Array<CommunityPostItem | HireItem | NewsItem>;
 }
 
-const CommunitySection = ({ title, to, data }: CommunitySectionProps) => {
+const CommunitySection = ({ children }: CommunitySectionProps) => {
+  return (
+    <div className="flex flex-col gap-4 text-sm xl:grid xl:grid-cols-2">
+      {children}
+    </div>
+  );
+};
+
+CommunitySection.List = ({ title, to, data }: CommunitySectionListProps) => {
   return (
     <Section>
       <Section.Header title={title}>
@@ -22,29 +34,14 @@ const CommunitySection = ({ title, to, data }: CommunitySectionProps) => {
       <Section.Body>
         {data.map((data, index) => (
           <ListButton key={index} to="">
-            {data.category && (
-              <p className="break-keep pr-4">{data.category}</p>
-            )}
             <p className="w-full truncate pr-4">{data.title}</p>
             <p className="text-clab-main-light">
-              {data.createAt ? toYYMMDD(data.createAt) : ''}
+              {data.createAt && toYYMMDD(data.createAt)}
             </p>
           </ListButton>
         ))}
       </Section.Body>
     </Section>
-  );
-};
-
-interface CommunityProps {
-  children: React.ReactNode;
-}
-
-CommunitySection.Wrapper = ({ children }: CommunityProps) => {
-  return (
-    <div className="flex flex-col gap-4 text-sm xl:grid xl:grid-cols-2">
-      {children}
-    </div>
   );
 };
 
