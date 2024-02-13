eaf7458f27a5eff9f0e50b6c0f7d11dee45a3ece
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-02-13 20:50:23 +0900

    refactor: Nav와 AlarmPanel에 Modal Component 사용하여 UI 개선

--- apps/member/src/components/panels/AlarmPanel/AlarmPanel.tsx
+++ apps/member/src/components/panels/AlarmPanel/AlarmPanel.tsx
@@ -1,9 +1,8 @@
-import { FcAbout } from 'react-icons/fc';
-
-import Panel from '@components/common/Panel/Panel';
-import SelectButton from '@components/common/SelectButton/SelectButton';
 import { useState } from 'react';
-import { NotificationItem } from '@type/notification';
+import { FcAbout } from 'react-icons/fc';
+import Panel from '@components/common/Panel/Panel';
+import useModal from '@hooks/common/useModal';
+import type { NotificationItem } from '@type/notification';
 
 interface AlarmPanelProps {
   data: Array<NotificationItem>;
@@ -11,9 +10,17 @@ interface AlarmPanelProps {
 
 const AlarmPanel = ({ data }: AlarmPanelProps) => {
   const [open, setOpen] = useState(true);
+  const { openModal } = useModal();
 
-  const handleClick = () => {
+  const handleOpen = () => {
     setOpen((prev) => !prev);
+  };
+
+  const onClickItem = (content: string) => {
+    openModal({
+      title: '알림',
+      content,
+    });
   };
 
   return (
@@ -23,17 +30,18 @@ const AlarmPanel = ({ data }: AlarmPanelProps) => {
         label="알림"
         description={`중요한 알림 ${data.length}`}
         isOpen={open}
-        onClick={handleClick}
+        onClick={handleOpen}
       />
       <Panel.Body isOpen={open} className="flex flex-col text-sm">
-        {data.map(
-          ({ id, content }) =>
-            id <= 5 && (
-              <SelectButton className="truncate" key={id} to="">
-                {content}
-              </SelectButton>
-            ),
-        )}
+        {data.slice(0, 5).map(({ id, content }) => (
+          <p
+            key={id}
+            onClick={() => onClickItem(content)}
+            className="rounded-md leading-relaxed transition hover:translate-x-1.5 hover:font-semibold truncate cursor-pointer"
+          >
+            {content}
+          </p>
+        ))}
       </Panel.Body>
     </Panel>
   );
