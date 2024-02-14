2c690ace3fcf57669909c6be25d2c53f37564390
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-02-14 23:31:20 +0900

    feat: 게시글 및 댓글 신고 시 토스트 알림 추가

--- apps/member/src/hooks/queries/useAccusesMutation.ts
+++ apps/member/src/hooks/queries/useAccusesMutation.ts
@@ -1,15 +1,16 @@
 import { postAccuses } from '@api/accuse';
-import { QUERY_KEY } from '@constants/key';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useToast from '@hooks/common/useToast';
+import { useMutation } from '@tanstack/react-query';
 
 export const useAccusesMutation = () => {
-  const queryClient = useQueryClient();
+  const toast = useToast();
 
   const AccusesPost = useMutation({
     mutationFn: postAccuses,
-    onSuccess: (boardId) => {
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.COMMUNITY, boardId],
+    onSuccess: () => {
+      toast({
+        state: 'success',
+        message: '신고가 완료되었습니다.',
       });
     },
   });
