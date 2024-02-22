60fb2b4a05f94e7230965301bfcc515c5c5ef052
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-02-23 02:17:44 +0900

    refactor: Table Component UI 개선

--- apps/member/src/components/common/Table/Table.tsx
+++ apps/member/src/components/common/Table/Table.tsx
@@ -16,7 +16,7 @@ const Table = ({ head, className, children }: TableProps) => {
   return (
     <table className={classNames('table-auto', className)}>
       <thead>
-        <tr className="bg-gray-100">
+        <tr className="bg-gray-100 text-center">
           {head.map((item, index) => (
             <th
               key={index}
@@ -36,7 +36,7 @@ Table.Row = ({ className, onClick, children }: PropsWithChildren<RowProps>) => {
   return (
     <tr
       className={classNames(
-        'items-center text-center cursor-pointer hover:bg-gray-100',
+        'items-center text-center hover:bg-gray-100',
         className,
       )}
       onClick={onClick}
