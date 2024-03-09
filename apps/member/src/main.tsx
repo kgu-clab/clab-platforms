c51018e8322573e29fda086d01c701ed59c07215
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-09 21:02:43 +0900

    chore: react StrictMode (#67)

--- apps/member/src/main.tsx
+++ apps/member/src/main.tsx
@@ -2,18 +2,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@styles/global.css';
 
+import { StrictMode } from 'react';
+import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@hooks/queries';
-import { RecoilRoot } from 'recoil';
 import ToastContainer from '@components/common/Toast/ToastContainer.tsx';
 import ModalContainer from '@components/common/Modal/ModalContainer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <RecoilRoot>
-      <App />
-      <ToastContainer />
-      <ModalContainer />
-    </RecoilRoot>
-  </QueryClientProvider>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <App />
+        <ToastContainer />
+        <ModalContainer />
+      </RecoilRoot>
+    </QueryClientProvider>
+  </StrictMode>,
 );
