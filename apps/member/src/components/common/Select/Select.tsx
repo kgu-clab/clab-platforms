3a9e744f6364b963c9720ff0b2cf0e39813f5653
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-26 20:52:57 +0900

    refactor(member): select component (#78)

--- apps/member/src/components/common/Select/Select.tsx
+++ apps/member/src/components/common/Select/Select.tsx
@@ -1,5 +1,5 @@
-import classNames from 'classnames';
 import { DEFAULT } from '@constants/default';
+import classNames from 'classnames';
 import { ComponentPropsWithRef } from 'react';
 
 interface SelectOptions {
@@ -8,24 +8,37 @@ interface SelectOptions {
 }
 
 interface SelectProps extends ComponentPropsWithRef<'select'> {
+  label?: string;
   options: readonly SelectOptions[];
 }
 
-const Select = ({ options = [], className, ...rest }: SelectProps) => {
+const Select = ({
+  options = [],
+  label,
+  className,
+  id,
+  ...rest
+}: SelectProps) => {
   return (
-    <select
-      className={classNames('border p-2 rounded-md', className)}
-      {...rest}
-    >
-      <option disabled value={DEFAULT.SELECT}>
-        미선택
-      </option>
-      {options.map(({ name, value }) => (
-        <option key={value} value={value}>
-          {name}
+    <div className={classNames('flex flex-col', className)}>
+      <label htmlFor={id} className="mb-1 ml-1 text-xs">
+        {label}
+      </label>
+      <select
+        id={id}
+        className="w-full h-full p-2 border rounded-md disabled:bg-gray-50"
+        {...rest}
+      >
+        <option disabled value={DEFAULT.SELECT}>
+          미선택
         </option>
-      ))}
-    </select>
+        {options.map(({ name, value }) => (
+          <option key={value} value={value}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
