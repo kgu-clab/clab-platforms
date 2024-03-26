157c983800c6a72676f74b99b5ec4f5f1ec2c77b
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-27 02:54:51 +0900

    refactor(member): add jsdocs to api, community (#78)

--- apps/member/src/utils/community.ts
+++ apps/member/src/utils/community.ts
@@ -1,7 +1,20 @@
-import type { CommunityCategoryType } from '@type/community';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type {
+  CommunityCategoryKorType,
+  CommunityCategoryType,
+} from '@type/community';
 import type { HireItem } from '@type/hire';
 
-export const categoryToTitle = (category: CommunityCategoryType) => {
+/**
+ * 주어진 커뮤니티 카테고리에 해당하는 한국어 제목을 반환합니다.
+ *
+ * @param {CommunityCategoryType} category - 커뮤니티 카테고리 (영문)
+ * @returns {CommunityCategoryKorType} 커뮤니티 카테고리에 해당하는 한국어 제목
+ * @throws {Error} 유효하지 않은 카테고리일 경우 에러 발생
+ */
+export function categoryToTitle(
+  category: CommunityCategoryType,
+): CommunityCategoryKorType {
   switch (category) {
     case 'notice':
       return '공지사항';
@@ -15,16 +28,58 @@ export const categoryToTitle = (category: CommunityCategoryType) => {
       return 'IT 뉴스';
     case 'hire':
       return '채용 정보';
+    default:
+      throw new Error('Invalid title');
   }
-};
+}
 
-export const isCommunityCategoryType = (
+/**
+ * 주어진 한국어 제목에 해당하는 커뮤니티 카테고리를 반환합니다.
+ *
+ * @param {CommunityCategoryKorType} title - 커뮤니티 카테고리 한국어 제목
+ * @returns {CommunityCategoryType} 한국어 제목에 해당하는 커뮤니티 카테고리 (영문)
+ * @throws {Error} 유효하지 않은 제목일 경우 에러 발생
+ */
+export function titleToCategory(
+  title: CommunityCategoryKorType,
+): CommunityCategoryType {
+  switch (title) {
+    case '공지사항':
+      return 'notice';
+    case '자유':
+      return 'free';
+    case 'QnA':
+      return 'qna';
+    case '졸업생':
+      return 'graduated';
+    case 'IT 뉴스':
+      return 'news';
+    case '채용 정보':
+      return 'hire';
+    default:
+      throw new Error('Invalid title');
+  }
+}
+
+/**
+ * 주어진 문자열이 유효한 커뮤니티 카테고리 타입인지 확인합니다.
+ *
+ * @param {string} type - 검사할 문자열
+ * @returns {boolean} 문자열이 유효한 커뮤니티 카테고리 타입이면 true, 그렇지 않으면 false
+ */
+export function isCommunityCategoryType(
   type: string,
-): type is CommunityCategoryType =>
-  ['notice', 'free', 'qna', 'graduated', 'news', 'hire'].includes(type);
+): type is CommunityCategoryType {
+  return ['notice', 'free', 'qna', 'graduated', 'news', 'hire'].includes(type);
+}
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isHireItem = (item: any): item is HireItem => {
+/**
+ * 주어진 객체가 유효한 채용 정보 아이템인지 확인합니다.
+ *
+ * @param {any} item - 검사할 객체
+ * @returns {boolean} 객체가 유효한 채용 정보 아이템이면 true, 그렇지 않으면 false
+ */
+export function isHireItem(item: any): item is HireItem {
   return (
     item &&
     item.careerLevel &&
@@ -32,4 +87,4 @@ export const isHireItem = (item: any): item is HireItem => {
     item.recruitmentPeriod &&
     item.jobPostingUrl
   );
-};
+}
