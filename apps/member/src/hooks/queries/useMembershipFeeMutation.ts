bd010662d753d5115fcc2f9a6d87a971a3ee969b
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-03-29 01:18:36 +0900

    refactor(member): useMembershipFeeMutation 에러 예외처리 (#78)

--- apps/member/src/hooks/queries/useMembershipFeeMutation.ts
+++ apps/member/src/hooks/queries/useMembershipFeeMutation.ts
@@ -1,5 +1,6 @@
 import { postMembershipFee } from '@api/membershipFee';
 import { QUERY_KEY } from '@constants/key';
+import { ERROR_MESSAGE } from '@constants/message';
 import useToast from '@hooks/common/useToast';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
@@ -12,11 +13,14 @@ export const useMembershipFeeMutation = () => {
 
   const membershipFeeMutation = useMutation({
     mutationFn: postMembershipFee,
-    onSuccess: (id) => {
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.MEMBERSHIP_FEE, id],
-      });
+    onSuccess: ({ success }) => {
+      if (!success) {
+        return toast({ state: 'error', message: ERROR_MESSAGE.DEFAULT });
+      }
       toast({ state: 'success', message: '신청이 완료되었습니다.' });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.MEMBERSHIP_FEE],
+      });
     },
   });
 
