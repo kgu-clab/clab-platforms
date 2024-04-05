cd156876c31f88c0f25a306ecb4e37b0904a675d
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-05 23:11:28 +0900

    feat(time): init project (#88)

---
+++ apps/time/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
+
+const inter = Noto_Sans_KR({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '경기타임',
+  description: '경기대학교에 계신 모든 순간을 도와드릴게요.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/time/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen">
+      경기타임
+    </main>
+  );
+}
