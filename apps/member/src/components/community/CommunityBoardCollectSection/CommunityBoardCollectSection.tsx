9a1e63fdac69f35f8f7076de1669d672bee7b931
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-06 20:55:31 +0900

    fix(member): hide writerId in `community collect` when in anonymous state (#86)

--- apps/member/src/components/community/CommunityBoardCollectSection/CommunityBoardCollectSection.tsx
+++ apps/member/src/components/community/CommunityBoardCollectSection/CommunityBoardCollectSection.tsx
@@ -1,10 +1,10 @@
 import Section from '@components/common/Section/Section';
 import { PATH_FINDER } from '@constants/path';
-import type { BoardItem } from '@type/board';
-import { titleToCategory } from '@utils/community';
+import { categoryToTitle } from '@utils/community';
 import { formattedDate } from '@utils/date';
 import { LiaCommentSolid } from 'react-icons/lia';
 import { Link } from 'react-router-dom';
+import type { BoardItem } from '@type/board';
 
 interface CommunityBoardCollectSectionProps {
   data: BoardItem[];
@@ -33,11 +33,11 @@ const CommunityBoardCollectSection = ({
           }) => (
             <Link
               key={id}
-              to={PATH_FINDER.COMMUNITY_POST(titleToCategory(category), id)}
+              to={PATH_FINDER.COMMUNITY_POST(category, id)}
               className="flex items-center gap-4 p-2 text-sm transition-colors border rounded-lg hover:border-clab-main-light text-nowrap"
             >
               <p className="flex items-center justify-center flex-shrink-0 font-semibold bg-gray-100 rounded-lg size-12">
-                {category}
+                {categoryToTitle(category)}
               </p>
               <div className="flex flex-col min-w-0 grow">
                 <p className="font-semibold truncate">{title}</p>
@@ -49,7 +49,7 @@ const CommunityBoardCollectSection = ({
               </p>
               <div className="flex flex-col">
                 <p>
-                  {writerName} ({writerId})
+                  {writerName} {writerId && `(${writerId})`}
                 </p>
                 <p>{formattedDate(createdAt)}</p>
               </div>
