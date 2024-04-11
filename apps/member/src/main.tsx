81581f6f8a4de68e94c9daadd4ab679bd621cc4f
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-11 23:32:25 +0900

    chore(member): add ChannelTalk (#100)

--- apps/member/src/main.tsx
+++ apps/member/src/main.tsx
@@ -1,13 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@styles/global.css';
+import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 
 import { StrictMode } from 'react';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@hooks/queries';
+import { CHANNEL_TALK_TOKEN } from '@constants/environment.ts';
 import ToastContainer from '@components/common/Toast/ToastContainer.tsx';
 import ModalContainer from '@components/common/Modal/ModalContainer.tsx';
+
+ChannelService.loadScript();
+ChannelService.boot({
+  pluginKey: CHANNEL_TALK_TOKEN,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
