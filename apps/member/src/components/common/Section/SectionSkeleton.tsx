e7c4eb25f68d127e6d33ff7b04a87265252d18b8
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-11 16:02:49 +0900

    feat(member): add SectionSkeleton Component (#93)

---
+++ apps/member/src/components/common/Section/SectionSkeleton.tsx
@@ -0,0 +1,11 @@
+import Section from './Section';
+
+const SectionSkeleton = () => {
+  return (
+    <Section className="w-full h-96 animate-pulse">
+      <Section.Header title=""></Section.Header>
+    </Section>
+  );
+};
+
+export default SectionSkeleton;
---
+++ apps/member/src/components/common/Section/index.ts
@@ -0,0 +1,2 @@
+export { default as Section } from './Section';
+export { default as SectionSkeleton } from './SectionSkeleton';
