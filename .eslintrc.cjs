2c2ec9240a4bb50a121c04652e2b1f58718b2531
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-12 22:45:43 +0900

    chore(*): updated eslint (#102)

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': 'error',
+  },
+};
