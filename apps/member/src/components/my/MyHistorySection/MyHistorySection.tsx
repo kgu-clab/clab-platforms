487c6843d40bb3a84bf751b5c3eafffcc6a862d0
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-13 20:35:23 +0900

    refactor(member): 마이페이지 데이터 표시 갯수 수정 (#106)

--- apps/member/src/components/my/MyHistorySection/MyHistorySection.tsx
+++ apps/member/src/components/my/MyHistorySection/MyHistorySection.tsx
@@ -2,9 +2,11 @@ import { useCallback } from 'react';
 
 import { Badge } from '@clab/design-system';
 
+import EmptyBox from '@components/common/EmptyBox/EmptyBox';
 import ListButton from '@components/common/ListButton/ListButton';
 import Section from '@components/common/Section/Section';
 
+import { MY_MESSAGE } from '@constants/message';
 import { PATH_FINDER } from '@constants/path';
 import useModal from '@hooks/common/useModal';
 import { toYYMMDD } from '@utils/date';
@@ -38,70 +40,79 @@ const MyHistorySection = ({ title, data }: MyHistorySectionProps) => {
     <Section>
       <Section.Header title={title} />
       <Section.Body className="text-sm">
-        {data.map((item) => {
-          /**
-           * 나의 댓글
-           */
-          if ('boardId' in item) {
-            const { id, boardId, boardCategory, content, createdAt } = item;
-            return (
-              <ListButton
-                key={id}
-                to={PATH_FINDER.COMMUNITY_POST(boardCategory, boardId)}
-              >
-                <p className="grow truncate pr-4">{content}</p>
-                <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
-              </ListButton>
-            );
-          }
-          /**
-           * 도서 대출 내역
-           */
-          if ('bookId' in item) {
-            const { bookId, bookTitle, borrowedAt, returnedAt } =
-              item as BookLoanRecordConditionType;
-            return (
-              <ListButton key={bookId} to={PATH_FINDER.BOOK_DETAIL(bookId)}>
-                <p className="grow space-x-2 truncate pr-4">
-                  <Badge color={returnedAt ? 'green' : 'yellow'}>
-                    {returnedAt ? '반납완료' : '대출중'}
-                  </Badge>
-                  <span>{bookTitle}</span>
-                </p>
-                <p className="text-clab-main-light">
-                  {toYYMMDD(borrowedAt ?? '')}
-                </p>
-              </ListButton>
-            );
-          }
-          /**
-           * 지난 알림
-           */
-          if ('content' in item) {
-            const { id, content, createdAt } = item as CommentItem;
-            return (
-              <ListButton key={id} onClick={() => handleAlarmClick(content)}>
-                <p className="grow truncate pr-4">{content}</p>
-                <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
-              </ListButton>
-            );
-          }
-          /**
-           * 나의 게시글
-           */
-          if ('title' in item) {
-            const { id, category, title, createdAt } = item as BoardItem;
-            return (
-              <ListButton
-                key={id}
-                to={PATH_FINDER.COMMUNITY_POST(category, id)}
-              >
-                <p className="grow truncate pr-4">{title}</p>
-                <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
-              </ListButton>
-            );
-          }
-        })}
+        {data.length === 0 ? (
+          <EmptyBox className="grow">{MY_MESSAGE.NO_HISTORY}</EmptyBox>
+        ) : (
+          data.map((item, index) => {
+            /**
+             * 나의 댓글
+             */
+            if ('boardId' in item) {
+              const { boardId, boardCategory, content, createdAt } = item;
+              return (
+                <ListButton
+                  key={createdAt}
+                  to={PATH_FINDER.COMMUNITY_POST(boardCategory, boardId)}
+                >
+                  <p className="grow truncate pr-4">{content}</p>
+                  <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
+                </ListButton>
+              );
+            }
+            /**
+             * 도서 대출 내역
+             */
+            if ('bookId' in item) {
+              const { bookId, bookTitle, borrowedAt, returnedAt } = item;
+              return (
+                <ListButton
+                  key={bookId + index}
+                  to={PATH_FINDER.BOOK_DETAIL(bookId)}
+                >
+                  <p className="grow space-x-2 truncate pr-4">
+                    <Badge color={returnedAt ? 'green' : 'yellow'}>
+                      {returnedAt ? '반납완료' : '대출중'}
+                    </Badge>
+                    <span>{bookTitle}</span>
+                  </p>
+                  <p className="text-clab-main-light">
+                    {toYYMMDD(borrowedAt ?? '')}
+                  </p>
+                </ListButton>
+              );
+            }
+            /**
+             * 지난 알림
+             */
+            if ('content' in item) {
+              const { content, createdAt } = item as CommentItem;
+              return (
+                <ListButton
+                  key={createdAt}
+                  onClick={() => handleAlarmClick(content)}
+                >
+                  <p className="grow truncate pr-4">{content}</p>
+                  <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
+                </ListButton>
+              );
+            }
+            /**
+             * 나의 게시글
+             */
+            if ('title' in item) {
+              const { id, category, title, createdAt } = item as BoardItem;
+              return (
+                <ListButton
+                  key={createdAt}
+                  to={PATH_FINDER.COMMUNITY_POST(category, id)}
+                >
+                  <p className="grow truncate pr-4">{title}</p>
+                  <p className="text-clab-main-light">{toYYMMDD(createdAt)}</p>
+                </ListButton>
+              );
+            }
+          })
+        )}
       </Section.Body>
     </Section>
   );
