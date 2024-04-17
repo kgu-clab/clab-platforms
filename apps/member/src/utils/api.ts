a57fbd57751d568f0cc005020206de943c685ac4
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-18 00:14:19 +0900

    feat(member): add createFormData function to handle file uploads (#108)

--- apps/member/src/utils/api.ts
+++ apps/member/src/utils/api.ts
@@ -1,8 +1,10 @@
 import {
   ACCESS_TOKEN_KEY,
+  FORM_DATA_KEY,
   REFRESH_TOKEN_KEY,
   SERVER_BASE_URL,
 } from '@constants/api';
+import { ERROR_MESSAGE } from '@constants/message';
 
 /**
  * 여러 개의 경로를 안전하게 연결하여 전체 경로를 생성합니다.
@@ -22,7 +24,6 @@ export function createPath(
     .join('')
     .replace(/([^:])\/\/+/g, '$1/');
 }
-
 /**
  * API 엔드포인트에 공통적인 페이징을 적용한 URL을 생성합니다.
  * @param {string} endpoint - 기본이 되는 API 엔드포인트 주소입니다.
@@ -44,7 +45,19 @@ export function createCommonPagination(
   });
   return url;
 }
-
+/**
+ * 파일을 FormData로 변환하는 함수입니다.
+ *
+ * @param file 변환할 파일
+ * @returns FormData 객체
+ * @throws {Error} 파일이 없을 경우 에러를 throw합니다.
+ */
+export function createFormData(file: File | null | undefined): FormData {
+  if (!file) throw new Error(ERROR_MESSAGE.NO_FILE);
+  const formData = new FormData();
+  formData.append(FORM_DATA_KEY, file);
+  return formData;
+}
 /**
  * 세션 스토리지에서 접근 토큰을 가져옵니다.
  * @returns {string | null} - 저장된 접근 토큰이 있으면 해당 문자열을, 없으면 null을 반환합니다.
@@ -70,7 +83,6 @@ export function setTokens(accessToken: string, refreshToken: string) {
   sessionStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
   sessionStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
 }
-
 /**
  * 세션 스토리지에서 접근 토큰과 갱신 토큰을 제거합니다.
  */
@@ -78,7 +90,6 @@ export function removeTokens() {
   sessionStorage.removeItem(ACCESS_TOKEN_KEY);
   sessionStorage.removeItem(REFRESH_TOKEN_KEY);
 }
-
 /**
  * 인증에 사용될 헤더 객체를 생성합니다.
  * @param {string | null} token - 사용할 토큰입니다. 토큰이 없다면 null을 전달합니다.
@@ -89,7 +100,6 @@ export function authorization(token: string | null): Record<string, string> {
     Authorization: `Bearer ${token}`,
   };
 }
-
 /**
  * 문자열이 base64 형식인지 확인합니다.
  * @param {string} url - 확인할 문자열입니다.
@@ -98,7 +108,6 @@ export function authorization(token: string | null): Record<string, string> {
 export function isBase64(url: string): boolean {
   return /;base64,/.test(url);
 }
-
 /**
  * 이미지 URL을 생성합니다. 문자열이 base64 형식이라면 그대로 반환하고, 아니라면 서버의 기본 URL과 경로를 조합합니다.
  * @param {string} imageUrl - 변환할 이미지 URL입니다.
