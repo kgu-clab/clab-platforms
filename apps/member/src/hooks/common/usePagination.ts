642af0631b2a127e6206558185de3b95e45f6642
gwansikk <seorit98@gmail.com>
2024-05-03 22:11:53 +0900

    refactor(member): optimize usePagination hook (#117)

--- apps/member/src/hooks/common/usePagination.ts
+++ apps/member/src/hooks/common/usePagination.ts
@@ -3,29 +3,32 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 /**
  * 페이지네이션을 위한 페이지 조작 훅입니다.
+ * Pagination Component와 같이 사용합니다.
  */
 export const usePagination = (defaultSize: number = 20) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
+
+  const getPage = useCallback(() => {
+    const searchParams = new URLSearchParams(location.search);
+    return parseInt(searchParams.get('page') ?? '1', 10) - 1;
+  }, [location.search]);
+
   const [pagination, setPagination] = useState({
-    page: parseInt(searchParams.get('page') ?? '1') - 1,
+    page: getPage(),
     size: defaultSize,
   });
 
-  // 페이지 변경 핸들러
   const handlePageChange = useCallback(
     (page: number) => {
-      navigate(`?page=${page}`, { replace: false });
+      navigate('?page=' + page);
     },
     [navigate],
   );
 
   useEffect(() => {
-    const page = parseInt(searchParams.get('page') ?? '1', 10) - 1;
-    setPagination((prev) => ({ ...prev, page }));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.search]);
+    setPagination((prev) => ({ ...prev, page: getPage() }));
+  }, [getPage]);
 
-  return { page: pagination.page, size: pagination.size, handlePageChange };
+  return { ...pagination, handlePageChange };
 };
