96dbde25283addfaf9e4af16f5df673f2fd8f4fe
gwansikk <seorit98@gmail.com>
2024-05-11 00:04:40 +0900

    chore(design-system): update vitest setup file path (#125)

--- packages/design-system/vitest.config.js
+++ packages/design-system/vitest.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setup.tests.ts'],
+    setupFiles: ['./vitest.setup.ts'],
     exclude: [...configDefaults.exclude, '**/node_modules/**'],
   },
 });
