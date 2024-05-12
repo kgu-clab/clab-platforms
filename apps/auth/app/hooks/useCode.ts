20f3964470eff2c1731094b823120b20cbf4e676
gwansikk <seorit98@gmail.com>
2024-05-12 17:35:08 +0900

    refactor(auth): replace useCode hook with useService hook (#129)

--- apps/auth/app/hooks/useCode.ts
+++
@@ -1,8 +0,0 @@
-import { useSearchParams } from 'next/navigation';
-
-export const useCode = () => {
-  const searchParams = useSearchParams();
-  const code = searchParams.get('code');
-
-  return { code };
-};
