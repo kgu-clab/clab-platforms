58ca686fa5b315330d313ec5d932bfec93e60739
gwansikk <seorit98@gmail.com>
2024-05-13 02:58:42 +0900

    refactor(design-system): update grid variants to use string literals (#133)

--- packages/design-system/src/Grid/Grid.styles.ts
+++ packages/design-system/src/Grid/Grid.styles.ts
@@ -1,18 +1,18 @@
 import { cva } from 'class-variance-authority';
 
-export const gridVariants = cva('gird', {
+export const gridVariants = cva('grid', {
   variants: {
     col: {
-      1: 'grid-cols-1',
-      2: 'grid-cols-2',
-      3: 'grid-cols-3',
-      4: 'grid-cols-4',
-      5: 'grid-cols-5',
-      6: 'grid-cols-6',
-      7: 'grid-cols-7',
-      8: 'grid-cols-8',
-      9: 'grid-cols-9',
-      10: 'grid-cols-10',
+      '1': 'grid-cols-1',
+      '2': 'grid-cols-2',
+      '3': 'grid-cols-3',
+      '4': 'grid-cols-4',
+      '5': 'grid-cols-5',
+      '6': 'grid-cols-6',
+      '7': 'grid-cols-7',
+      '8': 'grid-cols-8',
+      '9': 'grid-cols-9',
+      '10': 'grid-cols-10',
     },
     gap: {
       none: 'gap-0',
@@ -22,7 +22,7 @@ export const gridVariants = cva('gird', {
     },
   },
   defaultVariants: {
-    col: 1,
+    col: '1',
     gap: 'none',
   },
 });
