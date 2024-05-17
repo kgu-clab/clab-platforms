c444510927fbb65b2ead7d25dd223468be7eb48d
gwansikk <seorit98@gmail.com>
2024-05-18 02:41:56 +0900

    refactor(member): update file upload functions and storage periods (#142)

--- apps/member/src/api/uploadedFile.ts
+++ apps/member/src/api/uploadedFile.ts
@@ -1,92 +1,97 @@
-import { END_POINT, STORAGE_PERIOD } from '@constants/api';
+import { END_POINT, FORM_DATA_KEY, STORAGE_PERIOD } from '@constants/api';
 import { createPath } from '@utils/api';
 
-import type { AssignmentFileType } from '@type/activity';
-import type { BaseResponse, IDType } from '@type/api';
-import { ProfileImageFileType } from '@type/uploadFile';
+import type { BaseResponse, ResponseFile } from '@type/api';
 
 import { server } from './server';
 
-interface PostUploadedFileMembershipFee {
-  storagePeriod: number;
-  multipartFile: FormData;
+interface PostUploadedFileAssignmentParams {
+  groupId: number;
+  groupBoardId: number;
+  files: FormData;
 }
 
-interface PostUploadedFileAssignment {
-  groupId: IDType;
-  groupBoardId: IDType;
-  storagePeriod: number;
-  files: FormData;
+/**
+ * 파일 업로드
+ * @param files 업로드할 파일
+ * @param uploadedFetch 업로드할 파일을 전송하는 함수
+ */
+export async function uploadFiles<T>(
+  files: File[],
+  uploadedFetch: (multipartFile: FormData) => Promise<T>,
+) {
+  const formData = new FormData();
+  files.forEach((file) => {
+    formData.append(FORM_DATA_KEY, file);
+  });
+
+  return uploadedFetch(formData);
+}
+
+/**
+ * 게시글 파일 업로드
+ */
+export async function postUploadedFileBoard(multipartFile: FormData) {
+  const { data } = await server.post<FormData, BaseResponse<ResponseFile[]>>({
+    url: createPath(END_POINT.UPLOADEDFILE_BOARD, STORAGE_PERIOD(3)), // 게시글 파일은 3년간 보관
+    body: multipartFile,
+  });
+
+  return data;
 }
 /**
  * 회비 증빙 사진 업로드
  */
-export const postUploadedFileMembershipFee = async ({
-  storagePeriod,
-  multipartFile,
-}: PostUploadedFileMembershipFee) => {
+export async function postUploadedFileMembershipFee(multipartFile: FormData) {
   const url = createPath(
     END_POINT.UPLOADEDFILE_MEMBERSHIP_FEE,
-    STORAGE_PERIOD(storagePeriod),
+    STORAGE_PERIOD(5), // 회비 증빙 사진은 5년간 보관
   );
-  const { data } = await server.post<
-    FormData,
-    BaseResponse<AssignmentFileType[]>
-  >({
+  const { data } = await server.post<FormData, BaseResponse<ResponseFile[]>>({
     url: url,
     body: multipartFile,
   });
 
   return data;
-};
+}
 /**
  * 활동 그룹 과제 업로드
  */
-export const postUploadedFileAssignment = async ({
+export async function postUploadedFileAssignment({
   groupId,
   groupBoardId,
-  storagePeriod,
   files,
-}: PostUploadedFileAssignment) => {
+}: PostUploadedFileAssignmentParams) {
   const url = createPath(
     END_POINT.UPLOADEDFILE_ACTIVITY_ASSIGNMENT(groupId, groupBoardId),
-    STORAGE_PERIOD(storagePeriod),
+    STORAGE_PERIOD(3), // 활동 과제 파일은 3년간 보관
   );
-  const { data } = await server.post<
-    FormData,
-    BaseResponse<AssignmentFileType[]>
-  >({
+  const { data } = await server.post<FormData, BaseResponse<ResponseFile[]>>({
     url,
     body: files,
   });
 
   return data;
-};
+}
 /**
  * 멤버 프로필 사진 업로드
  */
-export const postUploadedFileProfileImage = async (multipartFile: FormData) => {
-  const { data } = await server.post<
-    FormData,
-    BaseResponse<ProfileImageFileType>
-  >({
-    url: createPath(END_POINT.UPLOADEDFILE_PROFILES, STORAGE_PERIOD()),
+export async function postUploadedFileProfileImage(multipartFile: FormData) {
+  const { data } = await server.post<FormData, BaseResponse<ResponseFile>>({
+    url: createPath(END_POINT.UPLOADEDFILE_PROFILES, STORAGE_PERIOD(6)), // 프로필 사진은 6년간 보관
     body: multipartFile,
   });
 
   return data;
-};
+}
 /**
  * 함께하는 활동 사진 업로드
  */
-export const postFilesActivityPhotos = async (multipartFile: FormData) => {
-  const { data } = await server.post<
-    FormData,
-    BaseResponse<AssignmentFileType[]>
-  >({
-    url: createPath(END_POINT.UPLOADEDFILE_ACTIVITY_PHOTO, STORAGE_PERIOD()),
+export async function postFilesActivityPhotos(multipartFile: FormData) {
+  const { data } = await server.post<FormData, BaseResponse<ResponseFile[]>>({
+    url: createPath(END_POINT.UPLOADEDFILE_ACTIVITY_PHOTO, STORAGE_PERIOD(10)), // 함께하는 활동 사진은 10년간 보관
     body: multipartFile,
   });
 
   return data;
-};
+}
