6398aed0d57259be9a4d8ac1df89dcb8fd6a69cc
gwansikk <seorit98@gmail.com>
2024-05-25 19:56:35 +0900

    feat(auth): add LoginSuccess Component (#146)

---
+++ apps/auth/src/components/Login/LoginSuccess/LoginSuccess.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+import { type AuthData } from '@/app/[code]/page';
+import { REDIRECT_WITH_TOKEN } from '@utils/service';
+
+interface LoginSuccessProps {
+  data: AuthData;
+}
+
+const LoginSuccess = ({ data }: LoginSuccessProps) => {
+  useEffect(() => {
+    window.location.href = REDIRECT_WITH_TOKEN(data.code, data.token!);
+  }, [data.code, data.token]);
+
+  return (
+    <div>
+      <p className="text-center font-semibold">환영합니다!</p>
+      <p>잠시 후 연결된 서비스로 이동됩니다...</p>
+    </div>
+  );
+};
+
+export default LoginSuccess;
