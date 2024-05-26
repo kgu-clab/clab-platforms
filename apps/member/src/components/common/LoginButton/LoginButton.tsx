d2728b8d0c3bcbff29ee6ce1f6a308eea8302a45
gwansikk <seorit98@gmail.com>
2024-05-26 23:27:45 +0900

    refactor(member): update LOGIN_URL (#150)

--- apps/member/src/components/common/LoginButton/LoginButton.tsx
+++ apps/member/src/components/common/LoginButton/LoginButton.tsx
@@ -1,21 +1,20 @@
-import { MODE } from '@constants/environment';
+import { LOGIN_URL, SERVICE_NAME } from '@constants/environment';
 
 import Image from '../Image/Image';
 
 const LoginButton = () => {
-  const handleClick = () => {
-    const code = MODE === 'production' ? 'play' : 'dev';
-    window.location.href = `https://auth.clab.page/?code=${code}`;
+  const handleLoginButtonClick = () => {
+    window.location.href = LOGIN_URL;
   };
 
   return (
     <button
-      onClick={handleClick}
+      onClick={handleLoginButtonClick}
       className="flex w-full max-w-xs items-center gap-4 rounded-md border border-black bg-[#292d32] px-4 py-2"
     >
       <Image src="/logo.webp" alt="C-Lab" width="w-8" height="h-8" />
       <span className="grow text-center font-semibold text-white">
-        C-Lab Auth로 로그인
+        {SERVICE_NAME} Auth로 로그인
       </span>
     </button>
   );
