c4b7d0fd14489c0cbb7dd0d188b10eefb2a4d38c
gwansikk <seorit98@gmail.com>
2024-06-09 21:33:57 +0900

    chore(member): add tanstack query dev-tool package (#152)

--- apps/member/src/main.tsx
+++ apps/member/src/main.tsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import ModalContainer from '@components/common/Modal/ModalContainer.tsx';
 import ToastContainer from '@components/common/Toast/ToastContainer.tsx';
 
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
-import { CHANNEL_TALK_TOKEN } from '@constants/environment.ts';
+import { CHANNEL_TALK_TOKEN, IS_DEVELOPMENT } from '@constants/environment.ts';
 import { queryClient } from '@hooks/queries';
 import '@styles/globals.css';
 
@@ -28,6 +29,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <ToastContainer />
         <ModalContainer />
       </RecoilRoot>
+      {IS_DEVELOPMENT && (
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-left"
+        />
+      )}
     </QueryClientProvider>
   </StrictMode>,
 );
