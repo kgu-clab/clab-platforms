94e693aedd04c2b5e8aa6c20fd6cff742cb9ff6a
gwansikk <seorit98@gmail.com>
2024-06-09 22:57:31 +0900

    chore(member): rename useCommentWriteMutation to useCommentCreateMutation (#152)

--- apps/member/src/hooks/queries/comment/useCommentCreateMutation.ts
+++ apps/member/src/hooks/queries/comment/useCommentCreateMutation.ts
@@ -1,34 +1,30 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { postCommentWrite } from '@api/comment';
-import { QUERY_KEY } from '@constants/key';
-import { API_ERROR_MESSAGE, ERROR_MESSAGE } from '@constants/message';
+import { COMMENT_QUERY_KEY } from '@constants/key';
+import { API_ERROR_MESSAGE } from '@constants/message';
 import useToast from '@hooks/common/useToast';
 
 /**
  * 댓글을 작성합니다.
  */
-export const useCommentWriteMutation = () => {
+export const useCommentCreateMutation = () => {
   const queryClient = useQueryClient();
   const toast = useToast();
 
   const mutation = useMutation({
     mutationFn: postCommentWrite,
-    onSuccess: ({ success, data: id, errorMessage }) => {
+    onSuccess: ({ success, errorMessage }, { boardId }) => {
       if (success) {
-        queryClient.invalidateQueries({ queryKey: [QUERY_KEY.COMMENTS, id] });
+        queryClient.invalidateQueries({
+          queryKey: COMMENT_QUERY_KEY.DETAIL(boardId),
+        });
       } else if (errorMessage) {
         toast({
           state: 'error',
           message: API_ERROR_MESSAGE[errorMessage],
         });
       }
-    },
-    onError: () => {
-      toast({
-        state: 'error',
-        message: ERROR_MESSAGE.NETWORK,
-      });
     },
   });
 
