16ec2df90abc838ce9d2a80db51edcf7070558da
gwansikk <Seorit98@gmail.com>
2024-06-18 15:39:19 +0900

    refactor(member): update environment mode constant (#155)

--- apps/member/src/constants/environment.ts
+++ apps/member/src/constants/environment.ts
@@ -1,23 +1,41 @@
 import memberConfig from '../../member.config';
 
-type Mode = 'development' | 'production';
+type TEnvironmentMode = 'development' | 'production';
+type TEnvironmentValue<T = string> = T | undefined;
 
 /**
  * 현재 런타임 모드를 가져옵니다.
  */
-export const MODE: Mode = import.meta.env.VITE_MODE;
+export const ENVIRONMENT_MODE: TEnvironmentValue<TEnvironmentMode> = import.meta
+  .env.MODE as TEnvironmentMode;
+
+/**
+ * 프로덕션 환경인지 확인합니다.
+ */
+export const IS_PRODUCTION = ENVIRONMENT_MODE === 'production';
+
 /**
  * 개발 환경인지 확인합니다.
  */
-export const IS_DEVELOPMENT = MODE === 'development';
+export const IS_DEVELOPMENT = ENVIRONMENT_MODE === 'development';
+
 /**
+ * 서비스 이름을 가져옵니다.
  * 데이터가 존재하지 않을 경우 서비스 이름으로 대체하기 위해 사용합니다.
  */
-export const SERVICE_NAME = memberConfig.name;
+export const SERVICE_NAME: TEnvironmentValue = memberConfig.name;
+
 /**
  * 채널톡 토큰을 가져옵니다.
  */
-export const CHANNEL_TALK_TOKEN = import.meta.env.VITE_CHANNEL_TALK_TOKEN;
+export const CHANNEL_TALK_TOKEN: TEnvironmentValue = import.meta.env
+  .VITE_CHANNEL_TALK_TOKEN;
+
+/**
+ * Sentry DSN을 가져옵니다.
+ */
+export const SENTRY_DSN: TEnvironmentValue = import.meta.env.VITE_SENTRY_DSN;
+
 /**
  * 로그인 페이지 URL을 가져옵니다.
  * 환경에 따라 다른 URL을 사용합니다.
