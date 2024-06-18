890cff609e0e58f5576aafa16645cd5985e688f9
gwansikk <Seorit98@gmail.com>
2024-06-18 15:42:50 +0900

    feat(member): add sentry setup (#155)

--- apps/member/src/main.tsx
+++ apps/member/src/main.tsx
@@ -12,16 +12,44 @@ import ModalContainer from '@components/common/Modal/ModalContainer.tsx';
 import ToastContainer from '@components/common/Toast/ToastContainer.tsx';
 
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
-import { CHANNEL_TALK_TOKEN, IS_DEVELOPMENT } from '@constants/environment.ts';
+import { API_BASE_URL } from '@constants/api.ts';
+import {
+  CHANNEL_TALK_TOKEN,
+  ENVIRONMENT_MODE,
+  IS_DEVELOPMENT,
+  IS_PRODUCTION,
+  SENTRY_DSN,
+} from '@constants/environment.ts';
 import { queryClient } from '@hooks/queries';
+import * as Sentry from '@sentry/react';
 import '@styles/globals.css';
 
 import App from './App.tsx';
 
-ChannelService.loadScript();
-ChannelService.boot({
-  pluginKey: CHANNEL_TALK_TOKEN,
-});
+/**
+ * Channel Talk
+ * 프로덕션 환경에서만 채널톡을 활성화해요.
+ */
+if (IS_PRODUCTION && CHANNEL_TALK_TOKEN) {
+  ChannelService.loadScript();
+  ChannelService.boot({
+    pluginKey: CHANNEL_TALK_TOKEN,
+  });
+}
+
+/**
+ * Sentry
+ * 프로덕션 환경에서만 Sentry를 활성화해요.
+ */
+if (IS_PRODUCTION && SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: ENVIRONMENT_MODE,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', API_BASE_URL],
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -31,12 +59,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <ToastContainer />
         <ModalContainer />
       </RecoilRoot>
-      {IS_DEVELOPMENT && (
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          buttonPosition="bottom-left"
-        />
-      )}
+      {IS_DEVELOPMENT && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 );
