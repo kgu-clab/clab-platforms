469183e02b56ac68b2f6158c302a621a7a83e748
SWARVY <swarvy0826@naver.com>
2024-06-23 23:30:16 +0900

    feat(status): create header & footer component (#171)

---
+++ apps/status/src/widgets/menu/ui/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="flex w-full items-center justify-between py-6">
+      <div className="flex items-center gap-x-2">
+        <h1 className="text-3xl font-bold">C-Lab Status</h1>
+      </div>
+    </div>
+  );
+}
