66a9cadc2a675c8989f8abe469a0bca9f70b317b
SWARVY <swarvy0826@naver.com>
2024-06-23 23:32:53 +0900

    feat(status): add servcie status section & shared types / constants (#171)

---
+++ apps/status/src/shared/constant/serviceList.ts
@@ -0,0 +1,11 @@
+export const SERVICE_LIST = {
+  Auth: 'https://auth.clab.page/',
+  Member: 'https://www.clab.page/',
+  Land: 'https:///play.clab.page/',
+};
+
+export const SERVICE_STATUS_LIST = {
+  Auth: true,
+  Member: false,
+  Land: true,
+};
---
+++ apps/status/src/shared/type/service.ts
@@ -0,0 +1,3 @@
+import { SERVICE_LIST } from '../constant';
+
+export type ServiceList = keyof typeof SERVICE_LIST;
