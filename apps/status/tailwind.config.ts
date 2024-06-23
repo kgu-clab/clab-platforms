1b444543bdd83850e87c0a7c48ede8769cb76679
SWARVY <swarvy0826@naver.com>
2024-06-23 23:34:15 +0900

    chore(status): update fonts and scrollbar styles (#171)

--- apps/status/tailwind.config.ts
+++ apps/status/tailwind.config.ts
@@ -1,17 +1,17 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: '320px',
+        ...defaultTheme.screens,
+      },
+      colors: {
+        'clab-primary': '#292c33',
+        'clab-secondary': '#a855f7',
       },
     },
   },
