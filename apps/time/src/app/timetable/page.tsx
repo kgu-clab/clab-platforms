ad9db1666980574f3904d2347926dbd9834842cf
SWARVY <swarvy0826@naver.com>
2024-08-09 16:15:57 +0900

    style(time): apply container style at PageLayout (#97)

--- apps/time/src/app/timetable/page.tsx
+++ apps/time/src/app/timetable/page.tsx
@@ -3,7 +3,7 @@ import { TimeTableLayout } from '@/widgets/time-table';
 
 export default function TimeTablePage() {
   return (
-    <PageLayout nav footer className="w-full space-y-4">
+    <PageLayout nav footer className="container w-full space-y-4">
       <TimeTableLayout />
     </PageLayout>
   );
