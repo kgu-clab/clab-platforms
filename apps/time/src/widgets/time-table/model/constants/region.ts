3b3cb40af98525dccb2139c5dc63d850fcd46525
SWARVY <swarvy0826@naver.com>
2024-08-17 23:33:20 +0900

    feat(time): add day status constant in region & add campus status constants / types (#97)

--- apps/time/src/widgets/time-table/model/constants/region.ts
+++ apps/time/src/widgets/time-table/model/constants/region.ts
@@ -3,4 +3,20 @@ export const REGION = {
   campus2: '서울',
 } as const;
 
+export const DAY_STATUS = {
+  day: '주간',
+  night: '야간',
+} as const;
+
+export const CAMPUS_STATUS = {
+  day: [
+    `${REGION.campus1}${DAY_STATUS.day}`,
+    `${REGION.campus2}${DAY_STATUS.day}`,
+  ],
+  night: [
+    `${REGION.campus1}${DAY_STATUS.night}`,
+    `${REGION.campus2}${DAY_STATUS.night}`,
+  ],
+} as const;
+
 export const REGION_VALUE_ARRAY = Object.values(REGION);
--- apps/time/src/widgets/time-table/types/day-status.ts
+++
@@ -1,5 +0,0 @@
-import { DAY_STATUS } from '@/widgets/time-table';
-
-type DayStatus = keyof typeof DAY_STATUS;
-
-export type { DayStatus };
