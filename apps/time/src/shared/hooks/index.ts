acdc9c8e920ad41f42d7fc8b2670053b7e79ad79
SWARVY <swarvy0826@naver.com>
2024-08-19 23:39:10 +0900

    feat(time): refactor `useLectureList` hook to use `useInfiniteQuery` for pagination

--- apps/time/src/shared/hooks/index.ts
+++ apps/time/src/shared/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useOutsideClick } from './useOutsideClick';
 export { default as useModalAction } from './useModalAction';
 export { default as useModalState } from './useModalState';
 export { default as useDebounce } from './useDebounce';
+export { default as useInfiniteScroll } from './useInfiniteScroll';
