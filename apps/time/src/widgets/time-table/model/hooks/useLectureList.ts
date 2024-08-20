7ce0d30d3cf1a5d6323789dde407d16800277c39
SWARVY <swarvy0826@naver.com>
2024-08-20 14:32:08 +0900

    refactor(time): simplify the `select` clause in `useLectureList`

--- apps/time/src/widgets/time-table/model/hooks/useLectureList.ts
+++ apps/time/src/widgets/time-table/model/hooks/useLectureList.ts
@@ -12,7 +12,7 @@ export default function useLectureList({ ...params }: GetLectureListParams) {
     queryFn: ({ pageParam }) =>
       getLectureList({ ...params, cursor: pageParam }),
     initialPageParam: 0,
-    select: (data) => (data.pages ?? []).flatMap((page) => page.data.values),
+    select: ({ pages = [] }) => pages.flatMap((page) => page.data.values),
     getNextPageParam: (lastPage) =>
       lastPage.data.hasNext
         ? lastPage.data.values[params.limit - 1].id
