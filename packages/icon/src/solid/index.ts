6fd74088ff5361a1c7592ba3f7dce29d6cbd4e83
SWARVY <swarvy0826@naver.com>
2024-08-28 12:02:22 +0900

    feat(icon): add `clipboard-document` & `clipboard-document-check` solid icons

--- packages/icon/src/solid/index.ts
+++ packages/icon/src/solid/index.ts
@@ -1,3 +1,5 @@
+export { default as ClipboardDocumentSolid } from './react/ClipboardDocument';
+export { default as ClipboardDocumentCheckSolid } from './react/ClipboardDocumentCheck';
 export { default as FacebookSolid } from './react/Facebook';
 export { default as LinkSolid } from './react/Link';
 export { default as MailSolid } from './react/Mail';
