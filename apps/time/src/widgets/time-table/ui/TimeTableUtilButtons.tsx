c610aba3c4f091458882940679763770b7302848
SWARVY <swarvy0826@naver.com>
2024-08-28 12:01:40 +0900

    feat(time): add url copy button

--- apps/time/src/widgets/time-table/ui/TimeTableUtilButtons.tsx
+++ apps/time/src/widgets/time-table/ui/TimeTableUtilButtons.tsx
@@ -1,3 +1,56 @@
+'use client';
+
+import { useState } from 'react';
+
+import {
+  ClipboardDocumentCheckSolid,
+  ClipboardDocumentSolid,
+} from '@clab-platforms/icon';
+
+function TimeTableCopyButton() {
+  const [isCopy, setIsCopy] = useState<boolean>(false);
+
+  const handleCopyButton = async () => {
+    try {
+      const url = window.location.href;
+      await navigator.clipboard.writeText(url);
+      setIsCopy(true);
+
+      setTimeout(() => {
+        setIsCopy(false);
+      }, 2000);
+    } catch (error) {
+      alert(`Failed to copy: ${error}`);
+    }
+  };
+
+  return (
+    <button
+      className="rounded-md border border-gray-400 bg-gray-50 p-3 transition-colors hover:bg-gray-100"
+      type="button"
+      onClick={handleCopyButton}
+    >
+      {!isCopy ? (
+        <ClipboardDocumentSolid
+          className="fill-gray-800"
+          width={24}
+          height={24}
+        />
+      ) : (
+        <ClipboardDocumentCheckSolid
+          className="fill-green-600"
+          width={24}
+          height={24}
+        />
+      )}
+    </button>
+  );
+}
+
 export default function TimeTableUtilButtons() {
-  return <div className="w-20"></div>;
+  return (
+    <div className="flex items-center justify-between">
+      <TimeTableCopyButton />
+    </div>
+  );
 }
