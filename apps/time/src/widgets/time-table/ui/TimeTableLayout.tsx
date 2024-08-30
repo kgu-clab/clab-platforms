8823a0677f1b4ffb88c43b02a1f1d4653a643a66
SWARVY <swarvy0826@naver.com>
2024-08-30 22:01:25 +0900

    style(time): change `TimeTableLayout` top/bottom padding

--- apps/time/src/widgets/time-table/ui/TimeTableLayout.tsx
+++ apps/time/src/widgets/time-table/ui/TimeTableLayout.tsx
@@ -4,7 +4,7 @@ import { TimeTable, TimeTableHeader } from '@/widgets/time-table/ui';
 
 export default function TimeTableLayout() {
   return (
-    <div className="flex flex-col items-center justify-center gap-y-8 py-20">
+    <div className="flex flex-col items-center justify-center gap-y-8 py-6">
       <TimeTableHeader />
       <TimeTable />
     </div>
