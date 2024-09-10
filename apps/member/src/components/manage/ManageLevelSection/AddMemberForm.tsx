b779041f71776668812596e0a9353f8bb3ec07a0
SWARVY <swarvy0826@naver.com>
2024-09-10 13:14:47 +0900

    refactor(member): reduce states by change states to object

--- apps/member/src/components/manage/ManageLevelSection/AddMemberForm.tsx
+++ apps/member/src/components/manage/ManageLevelSection/AddMemberForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import { Button, Input } from '@clab-platforms/design-system';
 
@@ -7,39 +7,44 @@ import Select from '@components/common/Select/Select.tsx';
 import { SELECT_OPTIONS } from '@constants/select.ts';
 import { useMemberAddMutation } from '@hooks/queries/member/useMemberAddMutation.ts';
 
+import { AddMemberRequestType } from '@type/manage.ts';
+
+const defaultMemberInfo: AddMemberRequestType = {
+  id: '',
+  password: '',
+  name: '',
+  address: '',
+  email: '',
+  contact: '',
+  department: '',
+  grade: 1,
+  birth: '',
+  interests: '',
+  githubUrl: '',
+  imageUrl: '',
+  studentStatus: 'CURRENT',
+};
+
 const AddMemberForm = () => {
-  const [id, setId] = useState<string>('');
-  const [name, setName] = useState<string>('');
-  const [address, setAddress] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-  const [major, setMajor] = useState<string>('');
-  const [grade, setGrade] = useState<number>(1);
-  const [birth, setBirth] = useState<string>('');
-  const [interest, setInterest] = useState<string>('NULL');
-  const [githubUrl, setGithubUrl] = useState<string>('');
-  const [studentStatus, setStudentStatus] = useState<string>('CURRENT');
+  const [userInput, setUserInput] =
+    useState<AddMemberRequestType>(defaultMemberInfo);
   const { memberAddMutation } = useMemberAddMutation();
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    const body = {
-      id,
-      password: '',
-      name,
-      email,
-      contact: phoneNumber,
-      department: major,
-      grade: grade,
-      birth,
-      address,
-      interests: interest,
-      githubUrl,
-      studentStatus,
-      imageUrl: '',
-    };
-
     e.preventDefault();
-    memberAddMutation(body);
+    memberAddMutation(userInput);
+  };
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setUserInput((prev) => ({
+      ...prev,
+      [e.target.name]:
+        e.target.name !== 'grade'
+          ? e.target.value
+          : SELECT_OPTIONS.GRADE[+e.target.value - 1].value,
+    }));
   };
 
   return (
@@ -49,7 +54,7 @@ const AddMemberForm = () => {
         id="학번"
         name="id"
         placeholder="학번을 입력해주세요."
-        onChange={(e) => setId(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Input
@@ -57,16 +62,16 @@ const AddMemberForm = () => {
         id="이름"
         name="name"
         placeholder="이름을 입력해주세요."
-        onChange={(e) => setName(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Input
         label="전화번호"
         id="전화번호"
-        name="phoneNumber"
+        name="contact"
         type="tel"
         placeholder="전화번호를 입력해주세요."
-        onChange={(e) => setPhoneNumber(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Input
@@ -75,24 +80,22 @@ const AddMemberForm = () => {
         name="email"
         type="email"
         placeholder="이메일을 입력해주세요."
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Input
         label="학과"
         id="학과"
-        name="major"
+        name="department"
         placeholder="학과를 입력해주세요."
-        onChange={(e) => setMajor(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Select
         label="구분"
         options={SELECT_OPTIONS.GRADE}
         name="studentStatus"
-        onChange={(e) =>
-          setGrade(SELECT_OPTIONS.GRADE[+e.target.value - 1].value)
-        }
+        onChange={handleInputChange}
       />
       <Input
         label="생년월일"
@@ -100,7 +103,7 @@ const AddMemberForm = () => {
         name="birth"
         type="date"
         placeholder="생년월일을 입력해주세요."
-        onChange={(e) => setBirth(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Input
@@ -108,27 +111,27 @@ const AddMemberForm = () => {
         id="주소"
         name="address"
         placeholder="주소를 입력해주세요."
-        onChange={(e) => setAddress(e.target.value)}
+        onChange={handleInputChange}
         required
       />
       <Select
         label="분야"
         options={SELECT_OPTIONS.MY_FIELD}
         name="interests"
-        onChange={(e) => setInterest(e.target.value)}
+        onChange={handleInputChange}
       />
       <Input
         label="깃허브 주소"
         id="깃허브 주소"
         name="githubUrl"
         placeholder="깃허브 주소를 입력해주세요."
-        onChange={(e) => setGithubUrl(e.target.value)}
+        onChange={handleInputChange}
       />
       <Select
         label="구분"
         options={SELECT_OPTIONS.STUDENT_STATUS}
         name="studentStatus"
-        onChange={(e) => setStudentStatus(e.target.value)}
+        onChange={handleInputChange}
       />
       <Button type="submit" className="mt-2 w-full">
         멤버 추가하기
