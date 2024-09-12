2aec7e65ccd950d8a14b4531e23a2ceaf0a6585b
Jeong-Ag <kjungu1105@gmail.com>
2024-09-12 21:03:28 +0900

    refactor(member): 그룹 상태 변경 시 확인 모달 추가

--- apps/member/src/components/manage/ManageActivitySection/ManageActivitySection.tsx
+++ apps/member/src/components/manage/ManageActivitySection/ManageActivitySection.tsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import { Menubar, Table } from '@clab-platforms/design-system';
 
 import ActionButton from '@components/common/ActionButton/ActionButton';
+import EmptyBox from '@components/common/EmptyBox/EmptyBox';
 import Pagination from '@components/common/Pagination/Pagination';
 import { Section } from '@components/common/Section';
 import ActivityInfoModal from '@components/modal/ActivityInfoModal/ActivityInfoModal';
+import CheckConfirmModal from '@components/modal/CheckConfirmModal/CheckConfirmModal';
 
 import { TABLE_HEAD } from '@constants/head';
+import { GROUP_MESSAGE } from '@constants/message';
 import { ACTIVITY_STATE } from '@constants/state';
 import useModal from '@hooks/common/useModal';
 import { usePagination } from '@hooks/common/usePagination';
@@ -23,7 +26,7 @@ const ManageActivitySection = () => {
   const [mode, setMode] = useState<ActivityGroupStatusType>(
     ACTIVITY_STATE.WAITING,
   );
-  const { openModal } = useModal();
+  const { openModal, closeModal } = useModal();
   const { page, size, handlePageChange } = usePagination({
     defaultSize: 6,
     sectionName: 'activity',
@@ -31,14 +34,14 @@ const ManageActivitySection = () => {
   const { activityGroupDeleteMutate } = useActivityGroupDeleteMutation();
   const { activityGroupStatusMutate } = useActivityGroupStatusMutation();
   const { data: groupData } = useActivityGroupMember({
+    status: mode,
     page,
     size,
-    status: mode,
   });
 
   const handleInfoButtonClick = (groupId: number) => {
     return openModal({
-      title: '멤버 정보',
+      title: '그룹 정보',
       content: <ActivityInfoModal id={groupId} />,
     });
   };
@@ -49,106 +52,158 @@ const ManageActivitySection = () => {
     id: number,
     status: ActivityGroupStatusType,
   ) => {
-    activityGroupStatusMutate({
-      activityGroupId: id,
-      activityGroupStatus: status,
+    openModal({
+      content: (
+        <CheckConfirmModal
+          message="승인하시겠습니까?"
+          handleConfirmButton={() => {
+            activityGroupStatusMutate({
+              activityGroupId: id,
+              activityGroupStatus: status,
+            });
+          }}
+          handleClose={closeModal}
+        />
+      ),
     });
   };
   const handleRejectButtonClick = (id: number) => {
-    activityGroupDeleteMutate(id);
+    openModal({
+      content: (
+        <CheckConfirmModal
+          message="거절하시겠습니까?"
+          handleConfirmButton={() => {
+            activityGroupDeleteMutate(id);
+          }}
+          handleClose={closeModal}
+        />
+      ),
+    });
   };
 
   const renderMode = {
     WAITING: (
       <Table head={TABLE_HEAD.ACTIVITY_GROUP_DETAIL}>
-        {groupData.items.map(({ leaders, name, subject, id }, index) => (
-          <Table.Row key={id}>
-            <Table.Cell>{index + 1}</Table.Cell>
-            <Table.Cell>{name}</Table.Cell>
-            <Table.Cell>{subject}</Table.Cell>
-            <Table.Cell>{`${leaders[0].name} (${leaders[0].id})`}</Table.Cell>
-            <Table.Cell className="space-x-2">
-              <ActionButton
-                color="blue"
-                onClick={() => handleInfoButtonClick(id)}
-              >
-                정보
-              </ActionButton>
-              <ActionButton
-                color="green"
-                onClick={() =>
-                  handleApproveButtonClick(id, ACTIVITY_STATE.PROGRESSING)
-                }
-              >
-                승인
-              </ActionButton>
-              <ActionButton
-                color="red"
-                onClick={() => handleRejectButtonClick(id)}
-              >
-                거절
-              </ActionButton>
+        {groupData.items.length ? (
+          groupData.items.map(({ leaders, name, subject, id }, index) => (
+            <Table.Row key={id}>
+              <Table.Cell>{index + 1}</Table.Cell>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{subject}</Table.Cell>
+              <Table.Cell>
+                {leaders ? `${leaders[0].name} (${leaders[0].id})` : '-'}
+              </Table.Cell>
+              <Table.Cell className="space-x-2">
+                <ActionButton
+                  color="blue"
+                  onClick={() => handleInfoButtonClick(id)}
+                >
+                  정보
+                </ActionButton>
+                <ActionButton
+                  color="green"
+                  onClick={() =>
+                    handleApproveButtonClick(id, ACTIVITY_STATE.PROGRESSING)
+                  }
+                >
+                  승인
+                </ActionButton>
+                <ActionButton
+                  color="red"
+                  onClick={() => handleRejectButtonClick(id)}
+                >
+                  거절
+                </ActionButton>
+              </Table.Cell>
+            </Table.Row>
+          ))
+        ) : (
+          <Table.Row>
+            <Table.Cell colSpan={TABLE_HEAD.ACTIVITY_GROUP_DETAIL.length}>
+              <EmptyBox>{GROUP_MESSAGE.NO_WAITING_ACTIVITY}</EmptyBox>
             </Table.Cell>
           </Table.Row>
-        ))}
+        )}
       </Table>
     ),
     PROGRESSING: (
       <Table head={TABLE_HEAD.ACTIVITY_GROUP_DETAIL}>
-        {groupData.items.map(({ leaders, name, subject, id }, index) => (
-          <Table.Row key={id}>
-            <Table.Cell>{index + 1}</Table.Cell>
-            <Table.Cell>{name}</Table.Cell>
-            <Table.Cell>{subject}</Table.Cell>
-            <Table.Cell>{`${leaders[0].name} (${leaders[0].id})`}</Table.Cell>
-            <Table.Cell className="space-x-2">
-              <ActionButton
-                color="blue"
-                onClick={() => handleInfoButtonClick(id)}
-              >
-                정보
-              </ActionButton>
-              <ActionButton
-                color="red"
-                onClick={() => handleRejectButtonClick(id)}
-              >
-                삭제
-              </ActionButton>
-              <ActionButton
-                color="green"
-                onClick={() => handleApproveButtonClick(id, ACTIVITY_STATE.END)}
-              >
-                종료
-              </ActionButton>
+        {groupData.items.length ? (
+          groupData.items.map(({ leaders, name, subject, id }, index) => (
+            <Table.Row key={id}>
+              <Table.Cell>{index + 1}</Table.Cell>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{subject}</Table.Cell>
+              <Table.Cell>
+                {leaders ? `${leaders[0].name} (${leaders[0].id})` : '-'}
+              </Table.Cell>
+              <Table.Cell className="space-x-2">
+                <ActionButton
+                  color="blue"
+                  onClick={() => handleInfoButtonClick(id)}
+                >
+                  정보
+                </ActionButton>
+                <ActionButton
+                  color="red"
+                  onClick={() => handleRejectButtonClick(id)}
+                >
+                  삭제
+                </ActionButton>
+                <ActionButton
+                  color="green"
+                  onClick={() =>
+                    handleApproveButtonClick(id, ACTIVITY_STATE.END)
+                  }
+                >
+                  종료
+                </ActionButton>
+              </Table.Cell>
+            </Table.Row>
+          ))
+        ) : (
+          <Table.Row>
+            <Table.Cell colSpan={TABLE_HEAD.ACTIVITY_GROUP_DETAIL.length}>
+              <EmptyBox>{GROUP_MESSAGE.NO_PROGRESSING_ACTIVITY}</EmptyBox>
             </Table.Cell>
           </Table.Row>
-        ))}
+        )}
       </Table>
     ),
     END: (
       <Table head={TABLE_HEAD.ACTIVITY_GROUP_DETAIL}>
-        {groupData.items.map(({ leaders, name, subject, id }, index) => (
-          <Table.Row key={id}>
-            <Table.Cell>{index + 1}</Table.Cell>
-            <Table.Cell>{name}</Table.Cell>
-            <Table.Cell>{subject}</Table.Cell>
-            <Table.Cell>{`${leaders[0].name} (${leaders[0].id})`}</Table.Cell>
-            <Table.Cell className="space-x-2">
-              <ActionButton
-                color="blue"
-                onClick={() => handleInfoButtonClick(id)}
-              >
-                정보
-              </ActionButton>
-              <ActionButton
-                color="red"
-                onClick={() => handleRejectButtonClick(id)}
-              >
-                삭제
-              </ActionButton>
+        {groupData.items.length ? (
+          groupData.items.map(({ leaders, name, subject, id }, index) => (
+            <Table.Row key={id}>
+              <Table.Cell>{index + 1}</Table.Cell>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{subject}</Table.Cell>
+              <Table.Cell>
+                {leaders ? `${leaders[0].name} (${leaders[0].id})` : '-'}
+              </Table.Cell>
+              <Table.Cell className="space-x-2">
+                <ActionButton
+                  color="blue"
+                  onClick={() => handleInfoButtonClick(id)}
+                >
+                  정보
+                </ActionButton>
+                <ActionButton
+                  color="red"
+                  onClick={() => handleRejectButtonClick(id)}
+                >
+                  삭제
+                </ActionButton>
+              </Table.Cell>
+            </Table.Row>
+          ))
+        ) : (
+          <Table.Row>
+            <Table.Cell colSpan={TABLE_HEAD.ACTIVITY_GROUP_DETAIL.length}>
+              <EmptyBox>{GROUP_MESSAGE.NO_END_ACTIVITY}</EmptyBox>
             </Table.Cell>
           </Table.Row>
-        ))}
+        )}
       </Table>
     ),
   }[mode];
