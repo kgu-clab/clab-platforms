cc45f79177e1b6ef41d984900affabe1a9ad61eb
Jeong-Ag <kjungu1105@gmail.com>
2024-09-12 20:49:24 +0900

    feat(member): 활동 지원 멤버 일괄 승인 기능 추가

--- apps/member/src/components/group/ActivityParticipantEditor/ActivityParticipantEditor.tsx
+++ apps/member/src/components/group/ActivityParticipantEditor/ActivityParticipantEditor.tsx
@@ -1,46 +1,86 @@
 import { useState } from 'react';
 
-import { Button, Table } from '@clab-platforms/design-system';
+import {
+  Badge,
+  BadgeColorVariant,
+  Button,
+  Checkbox,
+  Menubar,
+  Table,
+} from '@clab-platforms/design-system';
 
+import EmptyBox from '@components/common/EmptyBox/EmptyBox';
+import Pagination from '@components/common/Pagination/Pagination';
 import Section from '@components/common/Section/Section';
-import Select from '@components/common/Select/Select';
+import CheckConfirmModal from '@components/modal/CheckConfirmModal/CheckConfirmModal';
 
 import { TABLE_HEAD } from '@constants/head';
+import { GROUP_MESSAGE } from '@constants/message';
 import { ACTIVITY_MEMBER_ROLE, ACTIVITY_MEMBER_STATE } from '@constants/state';
 import useModal from '@hooks/common/useModal';
+import { usePagination } from '@hooks/common/usePagination';
 import {
   useActivityGroupApplication,
   useActivityGroupApplicationMutation,
   useActivityGroupMemberRoleMutation,
 } from '@hooks/queries';
-import { toKoreaActivityGroupMemberLevel } from '@utils/string';
+import {
+  toKoreaActivityGroupMemberLevel,
+  toKoreaActivityGroupMemberStatus,
+} from '@utils/string';
 
-import type { ActivityMemberRoleType } from '@type/activity';
+import type { ActivityMemberRoleType, MemberStatusType } from '@type/activity';
 
 interface ActivityParticipantEditorProps {
   groupId: number;
 }
 
-const memberOptions = Object.keys(ACTIVITY_MEMBER_ROLE).map((key) => ({
-  name: toKoreaActivityGroupMemberLevel(key as ActivityMemberRoleType),
-  value: key,
-}));
+interface ChangeRoleProps {
+  memberId: string;
+  status: boolean;
+}
+
+function getStatusColor(state: MemberStatusType): BadgeColorVariant {
+  switch (state) {
+    case ACTIVITY_MEMBER_STATE.ACCEPTED:
+      return 'green';
+    case ACTIVITY_MEMBER_STATE.REJECTED:
+      return 'red';
+    case ACTIVITY_MEMBER_STATE.WAITING:
+      return 'yellow';
+  }
+}
+function getRoleColor(state: ActivityMemberRoleType): BadgeColorVariant {
+  switch (state) {
+    case ACTIVITY_MEMBER_ROLE.MEMBER:
+      return 'secondary';
+    case ACTIVITY_MEMBER_ROLE.LEADER:
+      return 'blue';
+  }
+}
 
 const ActivityParticipantEditor = ({
   groupId,
 }: ActivityParticipantEditorProps) => {
-  const [role, setRole] = useState<ActivityMemberRoleType>(
-    ACTIVITY_MEMBER_ROLE.MEMBER,
-  );
+  const [mode, setMode] = useState(false);
+  const { page, size, handlePageChange } = usePagination({ defaultSize: 10 });
+  const { openModal, closeModal } = useModal();
   const { activityGroupApplicationMutate } =
     useActivityGroupApplicationMutation();
   const { data: applyMemberList } = useActivityGroupApplication({
     activityGroupId: groupId,
+    page,
+    size,
   });
   const { activityGroupMemberRoleMutate } =
     useActivityGroupMemberRoleMutation();
-  const { openModal } = useModal();
-
+  const [selectedMember, setSelectedMember] = useState<Array<ChangeRoleProps>>(
+    () =>
+      Array.from(applyMemberList.items).map((member) => ({
+        memberId: member.memberId,
+        status: false,
+      })),
+  );
   const handleOpenModal = (name: string, content: string) => {
     openModal({
       title: '📝 ' + name,
@@ -51,7 +91,7 @@ const ActivityParticipantEditor = ({
   const handleAcceptClick = (applierId: string) => {
     activityGroupApplicationMutate({
       activityGroupId: groupId,
-      memberId: applierId,
+      memberId: [applierId],
       status: ACTIVITY_MEMBER_STATE.ACCEPTED,
     });
   };
@@ -59,80 +99,158 @@ const ActivityParticipantEditor = ({
   const handleRejectClick = (applierId: string) => {
     activityGroupApplicationMutate({
       activityGroupId: groupId,
-      memberId: applierId,
+      memberId: [applierId],
       status: ACTIVITY_MEMBER_STATE.REJECTED,
     });
   };
 
-  const handleLeaderClick = (memberId: string) => {
+  const handleLeaderClick = (
+    memberId: string,
+    role: ActivityMemberRoleType,
+  ) => {
+    const changeRole =
+      role === ACTIVITY_MEMBER_ROLE.MEMBER
+        ? ACTIVITY_MEMBER_ROLE.LEADER
+        : ACTIVITY_MEMBER_ROLE.MEMBER;
+
     activityGroupMemberRoleMutate({
       activityGroupId: groupId,
-      memberId,
-      position: role,
+      memberId: memberId,
+      position: changeRole,
+    });
+  };
+
+  const handleChangeRoleToggle = (id: number) => {
+    setSelectedMember((prev) => {
+      const updateData = [...prev];
+      updateData[id] = { ...updateData[id], status: !updateData[id].status };
+      return updateData;
+    });
+  };
+
+  const handleMultipleChangeClick = () => {
+    const changeList = selectedMember
+      .filter((member) => member.status === true)
+      .map((member) => member.memberId);
+
+    openModal({
+      content: (
+        <CheckConfirmModal
+          message="변경하시겠습니까?"
+          handleConfirmButton={() => {
+            activityGroupApplicationMutate({
+              activityGroupId: groupId,
+              memberId: changeList,
+              status: ACTIVITY_MEMBER_STATE.ACCEPTED,
+            });
+          }}
+          handleClose={closeModal}
+        />
+      ),
     });
   };
 
   return (
     <Section>
-      <h1 className="pb-4 text-xl font-semibold">참여자 관리</h1>
-      {applyMemberList.items.length === 1 ? (
-        <div className="w-full rounded-lg border border-red-200 bg-red-50 p-5 text-center">
-          <p className="text-red-800">신청자가 없습니다.</p>
-        </div>
-      ) : (
-        <Table head={TABLE_HEAD.ACTIVITY_GROUP_APPLIES} className="w-full">
-          {applyMemberList.items.map(
-            ({ memberName, memberId, status, role, applyReason }, id) => (
-              <Table.Row key={id} className="text-center">
-                <Table.Cell>{id + 1}</Table.Cell>
-                <Table.Cell>{memberId}</Table.Cell>
-                <Table.Cell>{memberName}</Table.Cell>
-                <Table.Cell>{status}</Table.Cell>
-                <Table.Cell>{toKoreaActivityGroupMemberLevel(role)}</Table.Cell>
-                <Table.Cell className="space-x-2">
-                  <Button
-                    size="sm"
-                    onClick={() =>
-                      handleOpenModal(`${memberName}(${memberId})`, applyReason)
-                    }
-                  >
-                    지원서
-                  </Button>
-                  {status !== ACTIVITY_MEMBER_STATE.ACCEPTED && (
+      <Section.Header title="참여자 관리">
+        <Menubar>
+          <Menubar.Item selected={mode} onClick={() => setMode(!mode)}>
+            선택
+          </Menubar.Item>
+        </Menubar>
+      </Section.Header>
+      <Section.Body>
+        {applyMemberList.items.length === 1 ? (
+          <EmptyBox>{GROUP_MESSAGE.NO_PARTICIPANT}</EmptyBox>
+        ) : (
+          <Table head={TABLE_HEAD.ACTIVITY_GROUP_APPLIES} className="w-full">
+            {applyMemberList.items.map(
+              ({ memberName, memberId, status, role, applyReason }, id) => (
+                <Table.Row key={id} className="text-center">
+                  <Table.Cell>{id + 1}</Table.Cell>
+                  <Table.Cell>{memberId}</Table.Cell>
+                  <Table.Cell>{memberName}</Table.Cell>
+                  <Table.Cell>
+                    <Badge color={getStatusColor(status)}>
+                      {toKoreaActivityGroupMemberStatus(status)}
+                    </Badge>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Badge color={getRoleColor(role)}>
+                      {toKoreaActivityGroupMemberLevel(role)}
+                    </Badge>
+                  </Table.Cell>
+                  <Table.Cell className="space-x-2">
                     <Button
                       size="sm"
-                      color="green"
-                      onClick={() => handleAcceptClick(memberId)}
+                      onClick={() =>
+                        handleOpenModal(
+                          `${memberName}(${memberId})`,
+                          applyReason,
+                        )
+                      }
                     >
-                      승인
+                      지원서
                     </Button>
-                  )}
-                  <Button
-                    size="sm"
-                    color="red"
-                    onClick={() => handleRejectClick(memberId)}
-                  >
-                    거절
-                  </Button>
-                </Table.Cell>
-                <Table.Cell className="flex justify-center gap-1">
-                  <Select
-                    className="w-fit"
-                    id="role"
-                    onChange={(e) =>
-                      setRole(e.target.value as ActivityMemberRoleType)
-                    }
-                    options={memberOptions}
-                  />
-                  <Button onClick={() => handleLeaderClick(memberId)}>
-                    변경하기
-                  </Button>
-                </Table.Cell>
-              </Table.Row>
-            ),
-          )}
-        </Table>
-      )}
+                    {status !== ACTIVITY_MEMBER_STATE.ACCEPTED && (
+                      <Button
+                        size="sm"
+                        color="green"
+                        onClick={() => handleAcceptClick(memberId)}
+                      >
+                        승인
+                      </Button>
+                    )}
+                    <Button
+                      size="sm"
+                      color="red"
+                      onClick={() => handleRejectClick(memberId)}
+                    >
+                      거절
+                    </Button>
+                  </Table.Cell>
+                  <Table.Cell className="flex justify-center gap-1">
+                    {status !== ACTIVITY_MEMBER_STATE.ACCEPTED && mode ? (
+                      <Checkbox
+                        id={`changeRole ${id}`}
+                        name={`changeRole ${id}`}
+                        label="승인하기"
+                        checked={selectedMember[id].status}
+                        onChange={() => handleChangeRoleToggle(id)}
+                      />
+                    ) : (
+                      <Button
+                        size="sm"
+                        onClick={() => handleLeaderClick(memberId, role)}
+                      >
+                        {role === ACTIVITY_MEMBER_ROLE.MEMBER
+                          ? '리더로 변경하기'
+                          : '멤버로 변경하기'}
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ),
+            )}
+          </Table>
+        )}
+        {mode && (
+          <Button
+            className="mt-4 w-full"
+            size="sm"
+            onClick={handleMultipleChangeClick}
+          >
+            멤버 승인 처리하기
+          </Button>
+        )}
+        <Pagination
+          className="mt-4 justify-center"
+          totalItems={applyMemberList.totalItems}
+          postLimit={size}
+          onChange={handlePageChange}
+          page={page}
+        />
+      </Section.Body>
     </Section>
   );
 };
---
+++ apps/member/src/components/modal/CheckConfirmModal/CheckConfirmModal.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@clab-platforms/design-system';
+
+import Modal from '@components/common/Modal/Modal';
+
+interface CheckConfirmModalProps {
+  message: string;
+  handleConfirmButton: () => void;
+  handleClose: () => void;
+}
+
+const CheckConfirmModal = ({
+  message,
+  handleConfirmButton,
+  handleClose,
+}: CheckConfirmModalProps) => {
+  return (
+    <Modal>
+      <Modal.Header>확인</Modal.Header>
+      <Modal.Body>
+        <p>{message}</p>
+      </Modal.Body>
+      <Modal.Footer className="flex space-x-2">
+        <Button
+          className="w-full"
+          color="blue"
+          onClick={() => {
+            handleConfirmButton();
+            handleClose();
+          }}
+        >
+          확인
+        </Button>
+        <Button className="w-full" color="red" onClick={() => handleClose()}>
+          취소
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+export default CheckConfirmModal;
