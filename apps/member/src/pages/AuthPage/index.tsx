000773519f1fa288ec9221f19478a78bc371ec69
gwansikk <gwansik.kim@gwansik.dev>
2024-09-21 02:36:50 +0900

    refactor(member): Auth 새로운 프로젝트 구조 적용

--- apps/member/src/pages/AuthPage/index.tsx
+++ apps/member/src/pages/AuthPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { server } from '@api/server';
@@ -6,12 +6,12 @@ import { PATH } from '@constants/path';
 import { useSetIsLoggedInStore } from '@store/auth';
 import { authorization, removeTokens, setTokens } from '@utils/api';
 
-const AuthPage = () => {
+export default function AuthPage() {
   const [searchParams] = useSearchParams();
   const setIsLoggedIn = useSetIsLoggedInStore();
   const navigate = useNavigate();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     // URL 쿼리 파라미터에서 토큰을 가져옵니다.
     const accessToken = searchParams.get('a');
     const refreshToken = searchParams.get('r');
@@ -23,17 +23,13 @@ const AuthPage = () => {
       server.setHeaders(authorization(accessToken));
       setIsLoggedIn(true);
       navigate(PATH.MAIN);
-      return;
     } else {
       // 토큰이 없으면 로그인 페이지로 이동합니다.
       removeTokens();
       setIsLoggedIn(false);
       navigate(PATH.LOGIN);
-      return;
     }
   }, [navigate, searchParams, setIsLoggedIn]);
 
   return <h1>로그인 중...</h1>;
-};
-
-export default AuthPage;
+}
