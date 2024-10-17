0eb45b9621d04f0e29e3abb4cb7fa3702c1b4946
gwansikk <gwansik.kim@gwansik.dev>
2024-10-18 02:54:05 +0900

    feat(status): remove project

--- apps/status/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CurrentServices } from '@/src/widgets/service-status';
-
-export default function Home() {
-  return (
-    <div className="flex size-full flex-col items-start justify-center gap-y-10">
-      <CurrentServices />
-    </div>
-  );
-}
--- apps/status/src/widgets/service-status/ui/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CurrentServices } from './CurrentServices';
