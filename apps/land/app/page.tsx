6a0300580d3b69c9a7dfd2a89fddf537f8db32ad
Jeong-Ag <kjungu1105@gmail.com>
2024-11-13 20:46:06 +0900

    chore(land): set font and directory

--- apps/land/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>clab.page land</h1>
-    </main>
-  );
-}
