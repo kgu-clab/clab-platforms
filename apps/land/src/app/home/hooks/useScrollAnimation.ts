5101905d9f30cb5a37e9717a962bd58a48688d14
Jeong-Ag <kjungu1105@gmail.com>
2024-12-01 21:10:31 +0900

    refactor(land): add dynamic class selection

--- apps/land/src/app/home/hooks/useScrollAnimation.ts
+++ apps/land/src/app/home/hooks/useScrollAnimation.ts
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react';
 
-export default function useScrollAnimation() {
+export default function useScrollAnimation(targetClass: string) {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const element = document.querySelector('.scroll-fade');
-      const direction = element?.getBoundingClientRect();
+      const element = document.querySelector(`.${targetClass}`);
+      if (!element) return; // 요소가 없으면 반환
+      const direction = element.getBoundingClientRect();
       const isInScreen =
-        direction &&
-        direction.top < window.innerHeight - 300 &&
-        direction.bottom >= 0;
+        direction.top < window.innerHeight - 300 && direction.bottom >= 0;
 
       if (isInScreen) {
         setIsVisible(true);
@@ -25,7 +24,7 @@ export default function useScrollAnimation() {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [targetClass]);
 
   return isVisible;
 }
