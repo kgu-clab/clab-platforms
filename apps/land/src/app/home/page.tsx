eeeae67f65c41c5299b3abea7e510c56ecb6bd32
Jeong-Ag <kjungu1105@gmail.com>
2024-12-15 22:27:44 +0900

    style(land): set background image on last section

--- apps/land/src/app/home/page.tsx
+++ apps/land/src/app/home/page.tsx
@@ -131,12 +131,7 @@ export default function Home() {
 
         <div
           ref={valueRef}
-          className="flex size-full flex-wrap justify-center md:justify-start md:overflow-scroll md:overflow-y-hidden md:p-12 md:px-64"
-          style={{
-            scrollbarWidth: 'none',
-            msOverflowStyle: 'none',
-            whiteSpace: 'nowrap',
-          }}
+          className="scrollbar-hide flex size-full flex-wrap justify-center md:justify-start md:overflow-scroll md:overflow-y-hidden md:p-12 md:px-64"
         >
           <div className="hidden space-x-12 md:flex">
             {VALUES.map(({ keyword, description }) => (
@@ -238,21 +233,29 @@ export default function Home() {
           ))}
         </div>
       </Section>
-      <Section className="text-center">
-        <p className="mb-4 px-16 text-4xl font-bold leading-normal md:text-6xl md:leading-normal">
-          C-Lab과 함께라면
-          <span className="text-clab-yellow"> 두려울 게 없는 여정</span>
-        </p>
-        <p className="text-clab-dark-yellow mb-12 text-center text-xl font-bold leading-normal md:text-2xl">
-          지금은 모집기간이 아니에요. <br />
-          3월, 새로운 시작과 함께 만나요!
-        </p>
-        <Link
-          href={PATH.APPLICATION}
-          className="bg-clab-yellow border-clab-yellow hover:text-clab-yellow mx-auto rounded-full border px-10 py-4 text-xl font-bold text-black hover:bg-opacity-0"
-        >
-          이전 모집글 확인하기
-        </Link>
+      <Section className="relative text-center">
+        <div
+          className="absolute inset-0 h-screen w-screen bg-[url('/clab_picture.jpeg')] bg-cover bg-center opacity-40"
+          style={{
+            boxShadow: 'inset 0 0 150px rgba(0, 0, 0, 0.5)',
+          }}
+        />
+        <div className="z-5 relative">
+          <p className="mb-4 px-16 text-4xl font-bold leading-normal md:text-6xl md:leading-normal">
+            C-Lab과 함께라면
+            <span className="text-clab-yellow"> 두려울 게 없는 여정</span>
+          </p>
+          <p className="mb-12 text-center text-xl font-bold leading-normal md:text-2xl">
+            지금은 모집기간이 아니에요. <br />
+            3월, 새로운 시작과 함께 만나요!
+          </p>
+          <Link
+            href={PATH.APPLICATION}
+            className="bg-clab-yellow border-clab-yellow hover:text-clab-yellow mx-auto rounded-full border px-10 py-4 text-xl font-bold text-black hover:bg-opacity-0"
+          >
+            이전 모집글 확인하기
+          </Link>
+        </div>
       </Section>
     </PageLayout>
   );
