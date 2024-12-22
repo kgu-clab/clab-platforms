d53d3279d179e1affe113cb086df4c5f5976a95c
cho4u4o <cho4u4o@gmail.com>
2024-12-23 00:49:22 +0800

    feat(time): add sugang component

---
+++ apps/time/src/app/sugang/page.tsx
@@ -0,0 +1,9 @@
+import PageLayout from '@/app/PageLayout';
+
+export default function SugangPage() {
+  return (
+    <PageLayout nav footer className="container w-full space-y-4">
+      <p></p>
+    </PageLayout>
+  );
+}
