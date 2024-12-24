92d22bd8a74edace6b2a632fe19b5906f49e81ed
cho4u4o <cho4u4o@gmail.com>
2024-12-25 02:15:07 +0900

    feat(time): make hook to render contents by pages and modes

--- apps/time/src/widgets/sugang/ui/SugangContents.tsx
+++ apps/time/src/widgets/sugang/ui/SugangContents.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import SugangPageButtons from './SugangPageButtons';
+import { useContentByPage } from '@/widgets/sugang/model';
+import { Mode } from '@/widgets/sugang/types';
+import { SugangPageButtons, SugangTableView } from '@/widgets/sugang/ui';
 
 export default function SugangContents() {
+  const { mode, selected, setSelected } = useContentByPage();
   return (
-    <div className="flex size-full flex-col px-4 py-2">
-      <SugangPageButtons />
+    <div className="flex h-[calc(100vh-7.5rem)] flex-col gap-y-2 px-4 py-2 text-sm">
+      <SugangPageButtons selected={selected} setSelected={setSelected} />
+      <div className="grid h-full gap-y-4 pt-2">
+        <SugangTableView mode={mode as Mode} />
+        <SugangTableView mode="registrationList" />
+      </div>
     </div>
   );
 }
