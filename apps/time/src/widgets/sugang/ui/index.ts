e35a7c53c914dbdb32e5b98f3544e0c76d0a1448
cho4u4o <cho4u4o@gmail.com>
2024-12-25 02:17:06 +0900

    feat(time): make sugang table view component

--- apps/time/src/widgets/sugang/ui/index.ts
+++ apps/time/src/widgets/sugang/ui/index.ts
@@ -3,3 +3,4 @@ export { default as SugangLayout } from './SugangLayout';
 export { default as SugangSideBar } from './SugangSideBar';
 export { default as SugangContents } from './SugangContents';
 export { default as SugangPageButtons } from './SugangPageButtons';
+export { default as SugangTableView } from './SugangTableView';
