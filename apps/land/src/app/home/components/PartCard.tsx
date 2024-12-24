07334a10e6bde37d3f6010ea1c34e92f9c8dbdf2
Jeong-Ag <kjungu1105@gmail.com>
2024-12-24 22:38:41 +0900

    feat(land): create part section

---
+++ apps/land/src/app/home/components/PartCard.tsx
@@ -0,0 +1,13 @@
+interface PartCardProps {
+  name: string;
+  description: string;
+}
+
+export default function PartCard({ name, description }: PartCardProps) {
+  return (
+    <div className="from-clab-yellow-gray border-clab-dark-yellow flex flex-col rounded-xl border bg-gradient-to-t p-8 text-center">
+      <p className="mb-8 text-3xl font-bold">{name}</p>
+      <p className="text-lg leading-loose">{description}</p>
+    </div>
+  );
+}
---
+++ apps/land/src/types/part.ts
@@ -0,0 +1,5 @@
+export type Part = {
+  id: number;
+  name: string;
+  description: string;
+};
