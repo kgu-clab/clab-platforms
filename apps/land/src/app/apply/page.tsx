dfb8c7b1e42a8dabc7c78740ef4a2d110cba8efd
Jeong-Ag <kjungu1105@gmail.com>
2025-01-07 18:34:58 +0900

    feat(land): create apply process

---
+++ apps/land/src/app/apply/page.tsx
@@ -0,0 +1,26 @@
+import PageLayout from '@/app/PageLayout';
+
+import {
+  ApplyProcessSection,
+  FAQSection,
+  NowApplySection,
+  RecruitmentNoticeSection,
+} from './sections';
+
+export default function Apply() {
+  return (
+    <PageLayout
+      nav
+      footer
+      className="flex min-h-screen flex-col overflow-hidden"
+    >
+      <h1 className="pt-32 text-center text-6xl font-bold md:pl-32 md:text-start">
+        Join To <span className="text-clab-yellow">C-Lab!</span>
+      </h1>
+      <ApplyProcessSection />
+      <RecruitmentNoticeSection />
+      <FAQSection />
+      <NowApplySection />
+    </PageLayout>
+  );
+}
