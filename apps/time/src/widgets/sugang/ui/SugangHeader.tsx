36e6a2375571a456c9514855f608896fccd87b6e
cho4u4o <cho4u4o@gmail.com>
2025-01-13 17:05:08 +0900

    feat(time): change menu icon to button

--- apps/time/src/widgets/sugang/ui/SugangHeader.tsx
+++ apps/time/src/widgets/sugang/ui/SugangHeader.tsx
@@ -7,7 +7,10 @@ function SugangHeader() {
   return (
     <div className="flex w-full items-center border-b-2 p-4">
       <div className="flex items-center gap-x-10">
-        <SvgMenu fontSize={20} /> <KguLogo className="h-fit w-24" />
+        <button>
+          <SvgMenu fontSize={20} />
+        </button>{' '}
+        <KguLogo className="h-fit w-24" />
       </div>
     </div>
   );
