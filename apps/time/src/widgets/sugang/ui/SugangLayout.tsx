ea7eef0adc6bfefc4da22010c4fe2c4750e0ab88
cho4u4o <cho4u4o@gmail.com>
2025-01-15 16:42:15 +0900

    fix(time): fix due to CI error

--- apps/time/src/widgets/sugang/ui/SugangLayout.tsx
+++ apps/time/src/widgets/sugang/ui/SugangLayout.tsx
@@ -54,7 +54,8 @@ function Header({
     <div className="flex w-full items-center border-b-2 border-black/20 p-4">
       <div className="flex items-center gap-x-10">
         <button onClick={() => setOpened((prev) => !prev)}>
-          <SvgMenu fontSize={20} />{' '}
+          <SvgMenu fontSize={20} />
+          {'  '}
         </button>
         <KguLogo className="h-fit w-24" />
       </div>
