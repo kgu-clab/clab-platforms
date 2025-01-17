0d2cd9ca90ef2ee3aced24ae0d6a241c280a604c
Jeong-Ag <kjungu1105@gmail.com>
2025-01-17 22:02:17 +0900

    feat(land): put FAQSection data

--- apps/land/src/app/apply/sections/FAQSection.tsx
+++ apps/land/src/app/apply/sections/FAQSection.tsx
@@ -6,32 +6,12 @@ import { ArrowRightUpSolid } from '@clab-platforms/icon';
 import { cn } from '@clab-platforms/utils';
 
 import { Section } from '@/components';
-import { PATH } from '@/constants';
+import { FAQ, PATH } from '@/constants';
 import Link from 'next/link';
-
-const Question = [
-  {
-    question: '모집 인원은 몇 명인가요?',
-    answer:
-      ' 신규 모집의 경우, C-Lab은 학우들의 학습을 지원하기 위해 새로운 멤버의 수를 특별히 제한하고 있지 않아요. 열정을 가지고 활발하게 참여할 수 있는 사람을 찾고 있어요. 그러나 동아리 운영에 지장을 주거나 학습 분위기를 해치는 행위가 발생한다면, 해당 멤버는 동아리에서 제명될 수도 있어요. 다른 모집 단위의 경우, 인원이 명시되면 해당 인원 수에 맞춰 선발하지만, 우수한 지원자가 다수인 경우 더 많은 인원을 선발할 수도 있어요. 하지만 반대의 경우, 해당 인원보다 적게 뽑히는 경우도 생길 수 있어요.',
-  },
-  {
-    question: '면접에선 어떤 것들을 물어보나요?',
-    answer: '',
-  },
-  {
-    question: '타학과/복수전공/휴학생도 지원이 가능한가요?',
-    answer: '',
-  },
-  {
-    question: '기초적인 실력을 가추고 있어야만 지원이 가능할까요?',
-    answer: '',
-  },
-];
 
 export default function FAQSection() {
   const [open, setOpen] = useState(
-    Array.from({ length: Question.length }).fill(false),
+    Array.from({ length: FAQ.length }).fill(false),
   );
 
   const handleQuestionClick = (index: number) => {
@@ -49,19 +29,19 @@ export default function FAQSection() {
         자주 묻는 질문들이에요.
       </p>
       <div className="w-full space-y-6 px-8 md:px-64">
-        {Question.map(({ question, answer }, index) => (
+        {FAQ.map(({ id, question, answer }) => (
           <div
-            key={index}
+            key={id}
             className="bg-clab-light-gray break-keep rounded-lg p-4 hover:cursor-pointer md:p-6"
-            onClick={() => handleQuestionClick(index)}
+            onClick={() => handleQuestionClick(id)}
           >
             <p className="text-lg font-bold md:text-xl">
-              {index + 1}. {question}
+              {id}. {question}
             </p>
             <div
               className={cn(
                 'mt-8 leading-loose transition-all duration-300 ease-in-out',
-                open[index]
+                open[id]
                   ? 'opacity-100'
                   : 'absolute h-0 overflow-hidden opacity-0',
               )}
---
+++ apps/land/src/types/apply.ts
@@ -0,0 +1,5 @@
+export type Question = {
+  id: number;
+  question: string;
+  answer: string;
+};
