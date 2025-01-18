79a9e99405a6125c793348466f514aa1c1650fa8
Jeong-Ag <kjungu1105@gmail.com>
2025-01-18 23:15:01 +0900

    chore(land): set up access to the image on the server

--- apps/land/next.config.js
+++ apps/land/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SERVER_URL],
+  },
+};
 
 module.exports = nextConfig;
