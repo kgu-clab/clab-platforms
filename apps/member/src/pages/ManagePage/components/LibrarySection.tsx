bbd91287a192a7a0ac0a92a803d068bbfb519bfa
Jeong-Ag <kjungu1105@gmail.com>
2025-01-29 21:16:58 +0900

    feat(member): 관리 페이지에 도서 등록 탭 추가

--- apps/member/src/pages/ManagePage/components/LibrarySection.tsx
+++ apps/member/src/pages/ManagePage/components/LibrarySection.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
-import { Menubar, Table } from '@clab-platforms/design-system';
+import {
+  Button,
+  Grid,
+  Input,
+  Menubar,
+  Table,
+} from '@clab-platforms/design-system';
 
 import ActionButton from '@components/common/ActionButton/ActionButton';
 import Pagination from '@components/common/Pagination/Pagination';
@@ -8,25 +14,45 @@ import { Section } from '@components/common/Section';
 import BookLoanConditionStatusBadge from '@components/library/BookLoanConditionStatusBadge';
 
 import { TABLE_HEAD } from '@constants/head';
+import { ERROR_MESSAGE } from '@constants/message';
 import { usePagination } from '@hooks/common/usePagination';
+import useToast from '@hooks/common/useToast';
 import { useBookLoanRecordConditions } from '@hooks/queries';
 import { calculateDDay, formattedDate } from '@utils/date';
 
+import type { Book } from '@type/book';
+
 import { useBookLoanRecordApproveMutation } from '../hooks/useBookLoanRecordApproveMutation';
 import { useBookLoanRecordOverdue } from '../hooks/useBookLoanRecordOverdue';
+import { useBookRegisterMutation } from '../hooks/useBookRegisterMutation';
 import { useMemberInfoModal } from '../hooks/useMemberInfoModal';
 
-type Mode = 'condition' | 'overdue';
+type Mode = 'condition' | 'overdue' | 'register';
 
 export function LibrarySection() {
+  const toast = useToast();
   const { open } = useMemberInfoModal();
   const { page, size, handlePageChange } = usePagination({
     sectionName: 'library',
+  });
+  const [inputs, setInputs] = useState<Book>({
+    category: '',
+    title: '',
+    author: '',
+    publisher: '',
+    imageUrl: '',
+    reviewLinks: [],
+  });
+  const [links, setLinks] = useState({
+    yes24Url: '',
+    kyoboUrl: '',
+    aladinUrl: '',
   });
 
   const [mode, setMode] = useState<Mode>('condition');
 
   const { bookLoanRecordApproveMutate } = useBookLoanRecordApproveMutation();
+  const { bookRegisterMutate } = useBookRegisterMutation();
   const { data: bookLoanRecordCondition } = useBookLoanRecordConditions({
     hasPermission: true,
     isReturned: false,
@@ -48,6 +74,39 @@ export function LibrarySection() {
 
   const handleMemberInfoClick = (memberId: string) => {
     open({ memberId: memberId });
+  };
+
+  const handleInputsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLinksChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLinks((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleRegisterButtonClick = async () => {
+    const { category, title, author, publisher } = inputs;
+    if (!category || !title || !author || !publisher) {
+      return toast({
+        state: 'error',
+        message: ERROR_MESSAGE.NO_DATA,
+      });
+    }
+
+    const reviewLinkList = Object.values(links).filter((link) => link !== '');
+
+    setInputs((prev) => ({
+      ...prev,
+      reviewLinks: reviewLinkList,
+    }));
+
+    bookRegisterMutate(inputs);
   };
 
   const renderMode = {
@@ -111,13 +170,93 @@ export function LibrarySection() {
         )}
       </Table>
     ),
+    register: (
+      <div className="space-y-2">
+        <Grid gap="md" col="2">
+          <Input
+            label="카테고리"
+            id="category"
+            name="category"
+            placeholder="카테고리를 작성해주세요"
+            value={inputs.category}
+            onChange={handleInputsChange}
+          />
+          <Input
+            label="제목"
+            id="title"
+            name="title"
+            placeholder="제목을 작성해주세요"
+            value={inputs.title}
+            onChange={handleInputsChange}
+            required
+          />
+        </Grid>
+        <Grid gap="md" col="2">
+          <Input
+            label="저자"
+            id="author"
+            name="author"
+            placeholder="저자를 작성해주세요"
+            value={inputs.author}
+            onChange={handleInputsChange}
+          />
+          <Input
+            label="출판사"
+            id="publisher"
+            name="publisher"
+            placeholder="출판사를 작성해주세요"
+            value={inputs.publisher}
+            onChange={handleInputsChange}
+          />
+        </Grid>
+        <Input
+          label="이미지 URL"
+          id="imageUrl"
+          name="imageUrl"
+          placeholder="ex) https://image.yes24.com/goods/123161563/XL"
+          value={inputs.imageUrl}
+          onChange={handleInputsChange}
+        />
+        <Input
+          label="서점 링크 - yes24"
+          id="yes24Url"
+          name="yes24Url"
+          placeholder="ex) https://www.yes24.com/Product/Goods/7516911"
+          value={links.yes24Url}
+          onChange={handleLinksChange}
+        />
+        <Input
+          label="서점 링크 - 교보문고"
+          id="kyoboUrl"
+          name="kyoboUrl"
+          placeholder="ex) https://product.kyobobook.co.kr/detail/S000000935360"
+          value={links.kyoboUrl}
+          onChange={handleLinksChange}
+        />
+        <Input
+          label="서점 링크 - 알라딘"
+          id="aladinUrl"
+          name="aladinUrl"
+          placeholder="ex) https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=8960773433&start=pnaver_02"
+          value={links.aladinUrl}
+          onChange={handleLinksChange}
+        />
+        <Button
+          color="blue"
+          className="w-full"
+          onClick={handleRegisterButtonClick}
+        >
+          등록하기
+        </Button>
+      </div>
+    ),
   }[mode];
 
   return (
     <Section>
       <Section.Header
         title="도서관"
-        description="대여자와 연체자를 확인할 수 있어요"
+        description="대여자와 연체자를 확인하거나 신규 도서를 등록할 수 있어요"
       >
         <Menubar>
           <Menubar.Item
@@ -132,21 +271,29 @@ export function LibrarySection() {
           >
             연체
           </Menubar.Item>
+          <Menubar.Item
+            selected={mode === 'register'}
+            onClick={() => handleMenubarItemClick('register')}
+          >
+            등록
+          </Menubar.Item>
         </Menubar>
       </Section.Header>
       <Section.Body>
         {renderMode}
-        <Pagination
-          className="mt-4 justify-center"
-          page={page}
-          postLimit={size}
-          totalItems={
-            mode === 'condition'
-              ? bookLoanRecordCondition.totalItems
-              : bookLoanRecordOverdue.totalItems
-          }
-          onChange={handlePageChange}
-        />
+        {mode !== 'register' && (
+          <Pagination
+            className="mt-4 justify-center"
+            page={page}
+            postLimit={size}
+            totalItems={
+              mode === 'condition'
+                ? bookLoanRecordCondition.totalItems
+                : bookLoanRecordOverdue.totalItems
+            }
+            onChange={handlePageChange}
+          />
+        )}
       </Section.Body>
     </Section>
   );
