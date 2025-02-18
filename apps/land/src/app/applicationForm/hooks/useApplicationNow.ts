2b43938c4f3ef2ed81d92c4e967c590592ae7739
Jeong-Ag <kjungu1105@gmail.com>
2025-02-18 17:08:51 +0900

    refactor(land): `react-hook-form` 적용

--- apps/land/src/app/applicationForm/hooks/useApplicationNow.ts
+++ apps/land/src/app/applicationForm/hooks/useApplicationNow.ts
@@ -7,10 +7,10 @@ import { RECRUITMENT_QUERY_KEY } from '@/constants';
  * 지원 합격 여부를 조회합니다.
  */
 export function useApplicationNow() {
-  const { data, isError } = useQuery({
+  const { data, isError, isLoading } = useQuery({
     queryKey: RECRUITMENT_QUERY_KEY.NOW(),
     queryFn: getApplicationNow,
   });
 
-  return { data, isError };
+  return { data, isError, isLoading };
 }
