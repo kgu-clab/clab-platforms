b919fc7c7299db4c1556d8763a18595f1b25ae26
seoJing <tjwlsrb1021@naver.com>
2025-06-18 16:15:54 +0900

    refactor(member): support 페이지를 membershipFee로 리네이밍

---
+++ apps/member/src/components/membership/membershipFee/MembershipFeeHistorySection.tsx
@@ -0,0 +1,107 @@
+import { Table } from '@clab-platforms/design-system';
+
+import ActionButton from '@components/common/ActionButton/ActionButton';
+import Pagination from '@components/common/Pagination/Pagination';
+import Section from '@components/common/Section/Section';
+import MembershipStatusBadge from '@components/membership/membershipStatusBadge/MembershipStatusBadge';
+
+import { ROLE_LEVEL } from '@constants/state';
+import { usePagination } from '@hooks/common/usePagination';
+import { useMembershipInfoModal } from '@hooks/modal/useMembershipInfoModal';
+import { useMembershipFee, useMyProfile } from '@hooks/queries';
+import { formattedDate } from '@utils/date';
+import { formatMemberName } from '@utils/string';
+
+import { type MembershipFeeType } from '@type/membershipFee';
+
+const TITLE = '신쳥 내역';
+
+interface Props {
+  title?: string;
+  size?: number;
+  withPagination?: boolean;
+  hasPermission?: boolean;
+}
+
+export function MembershipFeeHistorySection({
+  title,
+  size: defaultSize,
+  withPagination,
+  hasPermission = false,
+}: Props) {
+  const { page, size, handlePageChange } = usePagination({
+    defaultSize,
+    sectionName: 'history',
+  });
+
+  const { open } = useMembershipInfoModal();
+  const { data: myProfile } = useMyProfile();
+  const { data: membershipFee } = useMembershipFee({
+    hasPermission,
+    page,
+    size,
+  });
+
+  const handleInfoClick = (data: MembershipFeeType) => {
+    open({
+      data: data,
+      hasPermission: myProfile.roleLevel >= ROLE_LEVEL.SUPER,
+    });
+  };
+
+  return (
+    <Section>
+      <Section.Header
+        title={title ?? TITLE}
+        description="최근에 신청된 회비 신청 내역이에요"
+      />
+      <Section.Body>
+        <Table head={['번호', '요청자', '구분', '상태', '신청일', '기능']}>
+          {membershipFee.items.map((membership) => (
+            <Table.Row
+              key={membership.id}
+              onClick={() => handleInfoClick(membership)}
+            >
+              <Table.Cell>{membership.id}</Table.Cell>
+              <Table.Cell>
+                {formatMemberName(membership.memberName, membership.memberId)}
+              </Table.Cell>
+              <Table.Cell> {membership.category}</Table.Cell>
+              <Table.Cell>
+                <MembershipStatusBadge status={membership.status} />
+              </Table.Cell>
+              <Table.Cell>{formattedDate(membership.createdAt)}</Table.Cell>
+              <Table.Cell>
+                <ActionButton
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleInfoClick(membership);
+                  }}
+                >
+                  정보
+                </ActionButton>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table>
+        {withPagination && (
+          <Pagination
+            className="mt-4 justify-center"
+            totalItems={membershipFee.totalItems}
+            postLimit={size}
+            onChange={handlePageChange}
+            page={page}
+          />
+        )}
+      </Section.Body>
+    </Section>
+  );
+}
+
+export function MembershipFeeHistorySectionSkeleton() {
+  return (
+    <Section className="h-96 animate-pulse">
+      <Section.Header title={TITLE} />
+    </Section>
+  );
+}
---
+++ apps/member/src/pages/MembershipFeePage/hooks/useMembershipFeeRequestForm.ts
@@ -0,0 +1,75 @@
+import { useCallback, useState } from 'react';
+
+import { SELECT_DEFAULT_OPTION } from '@constants/select';
+
+const INIT_CHECK_LIST = [false, false, false];
+
+const INIT_FORM_DATA = {
+  category: SELECT_DEFAULT_OPTION,
+  amount: 0,
+  content: '',
+  account: '',
+  file: null,
+};
+
+export interface MembershipFeeRequestData {
+  category: string;
+  amount: number;
+  content: string;
+  account: string;
+  file?: File | null;
+}
+
+export const useMembershipFeeRequestForm = () => {
+  const [formData, setFormData] =
+    useState<MembershipFeeRequestData>(INIT_FORM_DATA);
+  const [checkList, setCheckList] = useState<boolean[]>(INIT_CHECK_LIST);
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setFormData((prev) => ({
+        ...prev,
+        [name]: name === 'amount' ? parseFloat(value.replace(/,/g, '')) : value,
+      }));
+    },
+    [],
+  );
+
+  const handleTabsChange = useCallback((value: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      category: value,
+    }));
+  }, []);
+
+  const handleFileAccepted = useCallback((file?: File) => {
+    setFormData((prev) => ({
+      ...prev,
+      file,
+    }));
+  }, []);
+
+  const handleCheckboxChange = useCallback((index: number) => {
+    setCheckList((prev) => {
+      const next = [...prev];
+      next[index] = !prev[index];
+      return next;
+    });
+  }, []);
+
+  const resetFormData = useCallback(() => {
+    setFormData(INIT_FORM_DATA);
+    setCheckList(INIT_CHECK_LIST);
+  }, []);
+
+  return {
+    formData,
+    checkList,
+    handleCheckboxChange,
+    handleFileAccepted,
+    handleInputChange,
+    handleTabsChange,
+    resetFormData,
+  } as const;
+};
