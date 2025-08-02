fc9217472af4dd1415bb611f2cb2c7a1d9302fc0
seoJing <tjwlsrb1021@naver.com>
2025-08-02 23:55:38 +0900

    refactor(member): support  타입 중복 개선 및 통합

--- apps/member/src/types/support.ts
+++ apps/member/src/types/support.ts
@@ -2,28 +2,38 @@ export type SupportCategoryType = 'BUG' | 'INQUIRY';
 
 export type SupportCategoryKorType = '버그' | '문의';
 
+export interface Support {
+  id: number;
+  title: string;
+  name: string;
+  createdAt: string;
+  writerId: string | null; // 익명일 경우 null
+  status: 'PENDING' | 'COMPLETED';
+  category: SupportCategoryType;
+}
+
 export interface SupportWriteItem {
   category: SupportCategoryType;
   title: string;
+  id?: number;
   content: string;
   wantAnonymous: boolean;
   fileUrlList?: Array<string>;
   imageUrl?: string;
 }
+
 export interface SupportAnswerItem {
-  isAnswered: boolean;
-  answer?: string;
+  id?: number;
+  content?: string;
   responder?: string;
-  answeredAt?: string;
+  createdAt?: string;
 }
 
-export interface SupportItem extends SupportAnswerItem {
-  id: number;
-  title: string;
+export interface SupportDetail extends Support {
   content: string;
-  category: SupportCategoryType;
-  memberId: string | null;
-  memberName: string;
-  createdAt: string;
+  wantAnonymous: boolean;
+  fileUrlList?: Array<string>;
   imageUrl?: string;
+  answer?: SupportAnswerItem;
+  isOwner: boolean;
 }
