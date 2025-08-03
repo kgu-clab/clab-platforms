0e2dea348a6a1eeaca9d3e203dfb3efa7f6c7581
cho4u4o <cho4u4o@gmail.com>
2025-08-04 02:18:45 +0900

    feat(time): 시간표 페이지 배경 사진 추가

--- apps/time/src/widgets/time-table/ui/TimeTableContainer.tsx
+++ apps/time/src/widgets/time-table/ui/TimeTableContainer.tsx
@@ -2,12 +2,22 @@
 
 import { Suspense } from 'react';
 
-import { TimeTableLayout } from '@/widgets/time-table/ui';
+import Image from 'next/image';
+
+import TimeTableLayout from './TimeTableLayout';
 
 export default function TimeTableContainer() {
   return (
     <Suspense>
-      <TimeTableLayout />
+      <div className="relative h-full w-[598px] shrink-0 overflow-hidden p-10">
+        <Image
+          className="absolute inset-0 -z-10 object-center"
+          src="/images/time-table/time-table-bg.png"
+          alt="time-table-bg"
+          fill
+        />
+        <TimeTableLayout />
+      </div>
     </Suspense>
   );
 }
