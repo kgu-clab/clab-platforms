2267f7cf728789c5c1f5d567a34328e007b4fff1
cho4u4o <cho4u4o@gmail.com>
2025-08-04 02:07:48 +0900

    style(time): `TimeTableHeader` 디자인 변경

--- apps/time/src/widgets/time-table/ui/TimeTableHeader.tsx
+++ apps/time/src/widgets/time-table/ui/TimeTableHeader.tsx
@@ -1,13 +1,15 @@
 import { memo } from 'react';
 
-import { TimeTableFilter, TimeTableUtilButtons } from '@/widgets/time-table/ui';
+import { TimeTableFilter } from '@/widgets/time-table/ui';
 
 function TimeTableHeader() {
   return (
     <div className="flex w-full items-center justify-between">
-      <h2 className="text-xl font-bold">정규 시간표</h2>
+      <div className="flex items-center gap-2">
+        <h2 className="text-xl font-bold">25년 1학기</h2>
+        <h2 className="font-regular text-lg">강의 시간표 (n학점)</h2>
+      </div>
       <TimeTableFilter />
-      <TimeTableUtilButtons />
     </div>
   );
 }
