e49b1fdcb783db048ac41a2e5b5c583be63f647c
yummjin <cho4u4o@gmail.com>
2026-01-12 14:48:46 +0900

    feat(re-member): 내보내기 설정 및 버튼 스토리북 옵션 추가

--- apps/re-member/src/components/common/button/Button.stories.ts
+++ apps/re-member/src/components/common/button/Button.stories.ts
@@ -13,7 +13,7 @@ const meta = {
   argTypes: {
     size: {
       control: "check",
-      options: ["large"],
+      options: ["large", "small"],
     },
     color: {
       control: "select",
---
+++ apps/re-member/src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { default as Button } from "./button/Button";
+export { default as Chip } from "./chip/Chip";
