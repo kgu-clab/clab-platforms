a1e0d837cba2bdd76d2c456710ea1067b0483bfe
yummjin <cho4u4o@gmail.com>
2026-01-12 13:50:18 +0900

    feat(re-member): 칩 컴포넌트 추가

---
+++ apps/re-member/src/components/common/chip/chip.css.ts
@@ -0,0 +1,23 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const chipVariant = cva(
+  "focus:outline-none flex items-center justify-center px-md py-sm font-12 font-medium rounded-full",
+  {
+    variants: {
+      color: {
+        red: "bg-chip-red/10 text-chip-red",
+        yellow: "bg-chip-yellow/10 text-chip-yellow",
+        green: "bg-chip-green/10 text-chip-green",
+        purple: "bg-chip-purple/10 text-chip-purple",
+        primary: "bg-primary/10 text-primary",
+        disabled: "bg-gray-2 text-gray-4",
+      },
+    },
+    defaultVariants: {
+      color: "primary",
+    },
+  },
+);
+
+export type ChipVariants = VariantProps<typeof chipVariant>;
+export type ChipColor = ChipVariants["color"];
