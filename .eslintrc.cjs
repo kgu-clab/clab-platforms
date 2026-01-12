e7a2c7ceb9a376fa54698af5d4dea4ea2ccd6d5b
yummjin <cho4u4o@gmail.com>
2026-01-12 13:15:39 +0900

    chore(re-member): 프로젝트 기본 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,40 @@
+let tailwindcssConfig = [];
+
+try {
+  const fs = require('fs');
+  const path = require('path');
+
+  const reMemberPackageJson = path.join(
+    __dirname,
+    'apps/re-member/package.json',
+  );
+  if (fs.existsSync(reMemberPackageJson)) {
+    const pkg = JSON.parse(fs.readFileSync(reMemberPackageJson, 'utf8'));
+    const tailwindVersion =
+      pkg.dependencies?.tailwindcss || pkg.devDependencies?.tailwindcss;
+    const isV4 = tailwindVersion && /^[\^~]?4\./.test(tailwindVersion);
+    if (isV4) {
+      tailwindcssConfig = [];
+    } else {
+      tailwindcssConfig = ['plugin:tailwindcss/recommended'];
+    }
+  } else {
+    tailwindcssConfig = ['plugin:tailwindcss/recommended'];
+  }
+} catch (error) {
+  tailwindcssConfig = ['plugin:tailwindcss/recommended'];
+}
+
 module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
-    'plugin:tailwindcss/recommended',
+    ...tailwindcssConfig,
   ],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: ['apps/re-member/**'],
   settings: {
     react: {
       version: 'detect',
---
+++ apps/re-member/README.md
@@ -0,0 +1,1 @@
+## C-Lab member 리뉴얼 프로젝트, re-member
