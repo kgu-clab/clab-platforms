b09017e5a6359f146de610744a4a0280126cbaa4
yummjin <cho4u4o@gmail.com>
2026-01-15 02:47:16 +0900

    feat(re-member): 스터디 리스트 컴포넌트 구현

--- apps/re-member/src/pages/activity/ActivityStudyPage.tsx
+++ apps/re-member/src/pages/activity/ActivityStudyPage.tsx
@@ -1,17 +1,11 @@
+import { ActivityStudyList } from "@/components/activity";
 import { Section } from "@/components/common";
 
 export default function ActivityStudyPage() {
   return (
     <>
       <Section title="진행중인 스터디" className="px-gutter">
-        <Section.List>
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-          <div className="h-20 w-full rounded-xl bg-gray-200" />
-        </Section.List>
+        <ActivityStudyList />
       </Section>
     </>
   );
