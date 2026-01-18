1f4f7d7f132446000d4cc2b10aa2ebd03db70a60
seoJing <tjwlsrb1021@naver.com>
2026-01-18 13:42:11 +0900

    refactor(re-member): filter 내부로 Tab 컴포먼트 이동 및 Tab 네이밍을 Option으로 변경

--- apps/re-member/src/components/community/index.ts
+++ apps/re-member/src/components/community/index.ts
@@ -1,6 +1,4 @@
 export { default as CommunityPostList } from "./CommunityPostList";
-export { default as CommunityInformationTab } from "./CommunityInformationTab";
-export { default as CommunityQuestionTab } from "./CommunityQuestionTab";
 export { default as CommunityFilter } from "./CommunityFilter";
 export { default as CommunityWriteButton } from "./CommunityWriteButton";
 export * from "./communityPostDetail";
