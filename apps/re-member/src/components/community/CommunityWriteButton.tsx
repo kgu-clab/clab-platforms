096f26864aadba399d5dce063504ab9ac347f62a
seoJing <tjwlsrb1021@naver.com>
2026-01-18 13:42:53 +0900

    feat(re-member): post 생성 버튼을 아이콘으로 변경

--- apps/re-member/src/components/community/CommunityWriteButton.tsx
+++ apps/re-member/src/components/community/CommunityWriteButton.tsx
@@ -1,7 +1,9 @@
+import { IoAdd } from "react-icons/io5";
+
 export default function CommunityWriteButton() {
   return (
-    <button className="bg-primary absolute bottom-32 right-4 flex h-14 w-14 items-center justify-center rounded-full pb-1 text-5xl text-white shadow-2xl">
-      +
+    <button className="bg-primary absolute bottom-32 right-4 flex h-14 w-14 items-center justify-center rounded-full text-4xl text-white shadow-2xl">
+      <IoAdd />
     </button>
   );
 }
